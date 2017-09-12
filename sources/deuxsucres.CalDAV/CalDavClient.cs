b3a95e519908ae91444f1a4289e18ee53495bfe2
Yanos <ygrenier@ygrenier.com>
2017-09-12 19:19:44 +0200

    Add documentation. Add a CalDAV project.

---
+++ sources/deuxsucres.CalDAV/CalDavClient.cs
@@ -0,0 +1,20 @@
+﻿using deuxsucres.WebDAV;
+using System;
+using System.Net.Http;
+
+namespace deuxsucres.CalDAV
+{
+    /// <summary>
+    /// CalDAV client
+    /// </summary>
+    public class CalDavClient : WebDavClient
+    {
+        /// <summary>
+        /// Create a new client
+        /// </summary>
+        public CalDavClient(string uri, string userName = null, string password = null, HttpMessageHandler handler = null) 
+            : base(uri, userName, password, handler)
+        {
+        }
+    }
+}
