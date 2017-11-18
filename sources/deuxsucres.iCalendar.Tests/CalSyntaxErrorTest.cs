5eada5096d6e759e43cd007354a4bc3ce8a1ef94
Yanos <ygrenier@ygrenier.com>
2017-11-18 18:03:13 +0100

    Add CalSyntaxError.

---
+++ sources/deuxsucres.iCalendar.Tests/CalSyntaxErrorTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests
+{
+    public class CalSyntaxErrorTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var ex = new CalSyntaxError("test");
+            Assert.Equal("test", ex.Message);
+            Assert.Null(ex.InnerException);
+
+            var iex = new InvalidOperationException("test");
+            ex = new CalSyntaxError("test", iex);
+            Assert.Equal("test", ex.Message);
+            Assert.Same(iex, ex.InnerException);
+        }
+    }
+}
---
+++ sources/deuxsucres.iCalendar/CalSyntaxError.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar
+{
+    /// <summary>
+    /// Exception for syntax error
+    /// </summary>
+    public class CalSyntaxError : Exception
+    {
+        /// <summary>
+        /// Create a new exception with a message
+        /// </summary>
+        public CalSyntaxError(string message) : base(message) { }
+
+        /// <summary>
+        /// Create a new exception with a message and an inner exception
+        /// </summary>
+        public CalSyntaxError(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
