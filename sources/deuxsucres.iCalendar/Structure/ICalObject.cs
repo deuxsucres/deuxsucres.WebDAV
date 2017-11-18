762399ec9797f3ba6d9ca884ff044139c5d40686
Yanos <ygrenier@ygrenier.com>
2017-11-18 17:12:49 +0100

    Prepare calendar object structure.

---
+++ sources/deuxsucres.iCalendar/Structure/ICalObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar.Structure
+{
+    /// <summary>
+    /// Object representation
+    /// </summary>
+    public interface ICalObject
+    {
+        // TODO Uncomment when serialisation is ready
+        ///// <summary>
+        ///// Serialize the object
+        ///// </summary>
+        //void Serialize(ICalWriter writer);
+
+        ///// <summary>
+        ///// Deserialize the object
+        ///// </summary>
+        //void Deserialize(ICalReader reader);
+
+        /// <summary>
+        /// Name of the object
+        /// </summary>
+        string Name { get; }
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Structure/ICalProperty.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar.Structure
+{
+    /// <summary>
+    /// Property representation
+    /// </summary>
+    public interface ICalProperty
+    {
+        // TODO Uncomment when serialisation is ready
+        ///// <summary>
+        ///// Serialize the property
+        ///// </summary>
+        //void Serialize(ICalWriter writer);
+
+        ///// <summary>
+        ///// Deserialize the property
+        ///// </summary>
+        ///// <param name="reader">Reader</param>
+        ///// <param name="line">Line</param>
+        //void Deserialize(ICalReader reader, ContentLine line);
+
+        /// <summary>
+        /// Name of the property
+        /// </summary>
+        string Name { get; set; }
+
+    }
+}
