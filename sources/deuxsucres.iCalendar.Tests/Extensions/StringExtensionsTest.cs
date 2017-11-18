35df721cca0fb01456463976e349cc977ca67653
Yanos <ygrenier@ygrenier.com>
2017-11-18 14:15:15 +0100

    Create a project for the iCalendar tests.

---
+++ sources/deuxsucres.iCalendar.Tests/Extensions/StringExtensionsTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Extensions
+{
+    public class StringExtensionsTest
+    {
+        [Fact]
+        public void IsEqual()
+        {
+            Assert.True("Test".IsEqual("tesT"));
+            Assert.False("Test".IsEqual(null));
+            string s = null;
+            Assert.True(s.IsEqual(null));
+        }
+    }
+}
