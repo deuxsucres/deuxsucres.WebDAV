c4fb348e4b84a9201789cef927b5569ebb79c8eb
Yanos <ygrenier@ygrenier.com>
2017-11-19 08:17:49 +0100

    Add method extensions for serialization.

---
+++ sources/deuxsucres.iCalendar.Tests/Serialization/SerializationExtensionsTest.cs
@@ -0,0 +1,159 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Serialization
+{
+    public class SerializationExtensionsTest
+    {
+        [Fact]
+        public void CheckStrict()
+        {
+            bool strict = false;
+            var mReader = new Mock<ICalReader>();
+            mReader.SetupGet(r => r.StrictMode).Returns(() => strict);
+            var reader = mReader.Object;
+
+            bool invoked = false;
+            Action action = () => invoked = true;
+
+            reader.CheckStrict(() => true, action);
+            Assert.False(invoked);
+
+            invoked = false;
+            reader.CheckStrict(() => false, action);
+            Assert.False(invoked);
+
+            invoked = false;
+            reader.CheckStrict(null, action);
+            Assert.False(invoked);
+
+            strict = true;
+            invoked = false;
+            reader.CheckStrict(() => true, action);
+            Assert.False(invoked);
+
+            invoked = false;
+            reader.CheckStrict(() => false, action);
+            Assert.True(invoked);
+
+            invoked = false;
+            reader.CheckStrict(null, action);
+            Assert.False(invoked);
+        }
+
+        [Fact]
+        public void CheckSyntaxError()
+        {
+            bool strict = false;
+            var mReader = new Mock<ICalReader>();
+            mReader.SetupGet(r => r.StrictMode).Returns(() => strict);
+            var reader = mReader.Object;
+
+            reader.CheckSyntaxError(() => true, "error");
+            reader.CheckSyntaxError(() => true, () => "error");
+
+            reader.CheckSyntaxError(() => false, "error");
+            reader.CheckSyntaxError(() => false, () => "error");
+
+            reader.CheckSyntaxError(null, "error");
+            reader.CheckSyntaxError(null, () => "error");
+
+            strict = true;
+
+            reader.CheckSyntaxError(() => true, "error");
+            reader.CheckSyntaxError(() => true, () => "error");
+
+            Assert.Throws<CalSyntaxError>(() => reader.CheckSyntaxError(() => false, "error"));
+            Assert.Throws<CalSyntaxError>(() => reader.CheckSyntaxError(() => false, () => "error"));
+
+            reader.CheckSyntaxError(null, "error");
+            reader.CheckSyntaxError(null, () => "error");
+
+        }
+
+        [Fact]
+        public void MakePropertyParameter_Default()
+        {
+            Mock<ICalPropertyParameter> mProp = null;
+            ICalPropertyParameter prop = null;
+
+            var mReader = new Mock<ICalReader>();
+            mReader.Setup(r => r.CreateDefaultParameter(It.IsAny<string>()))
+                .Returns<string>(n =>
+                {
+                    if (mProp != null)
+                    {
+                        mProp.SetupGet(p => p.Name).Returns(n);
+                        prop = mProp.Object;
+                    }
+                    return prop;
+                });
+            var reader = mReader.Object;
+
+            var result = reader.MakePropertyParameter("PropName", "PropValue");
+            Assert.Null(result);
+
+            mProp = new Mock<ICalPropertyParameter>();
+            result = reader.MakePropertyParameter("PropName", "PropValue");
+            Assert.Null(result);
+            mProp.Verify(p => p.Deserialize(reader, "PropName", "PropValue"), Times.Once());
+
+            mProp = new Mock<ICalPropertyParameter>();
+            mProp.Setup(p => p.Deserialize(It.IsAny<ICalReader>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns<ICalReader, string, string>((r, n, v) =>
+                {
+                    return true;
+                });
+            result = reader.MakePropertyParameter("PropName", "PropValue");
+            Assert.NotNull(result);
+            Assert.Same(prop, result);
+            mProp.Verify(p => p.Deserialize(reader, "PropName", "PropValue"), Times.Once());
+
+            reader = null;
+            result = reader.MakePropertyParameter("PropName", "PropValue");
+            Assert.Null(result);
+        }
+
+        class TestPropertyParameter : ICalPropertyParameter
+        {
+            public bool Deserialize(ICalReader reader, string name, string value)
+            {
+                Name = name;
+                Value = value;
+                return !string.IsNullOrWhiteSpace(name);
+            }
+            public bool Serialize(ICalWriter writer, ContentLine line)
+            {
+                line.SetParam(Name, Value);
+                return true;
+            }
+            public string Name { get; set; }
+            public string Value { get; set; }
+        }
+
+        [Fact]
+        public void MakePropertyParameter_Generic()
+        {
+            var mReader = new Mock<ICalReader>();
+            var reader = mReader.Object;
+
+            var result = reader.MakePropertyParameter<TestPropertyParameter>("PropName", "PropValue");
+            Assert.NotNull(result);
+            Assert.Equal("PropName", result.Name);
+            Assert.Equal("PropValue", result.Value);
+
+            // If the property name is empty, the TestPropertyParameter.Deserialize() returns false
+            result = reader.MakePropertyParameter<TestPropertyParameter>("", "PropValue");
+            Assert.Null(result);
+        }
+
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Serialization/SerializationExtensions.cs
@@ -0,0 +1,66 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar
+{
+    /// <summary>
+    /// Extensions for serialization
+    /// </summary>
+    public static class SerializationExtensions
+    {
+
+        /// <summary>
+        /// Check error in a strict mode
+        /// </summary>
+        public static void CheckStrict(this ICalReader reader, Func<bool> check, Action action)
+        {
+            if (check?.Invoke() == false && reader?.StrictMode == true)
+                action?.Invoke();
+        }
+
+        /// <summary>
+        /// Check a syntax error
+        /// </summary>
+        public static void CheckSyntaxError(this ICalReader reader, Func<bool> check, string message)
+        {
+            reader.CheckStrict(check, () => { throw new CalSyntaxError(message); });
+        }
+
+        /// <summary>
+        /// Check a syntax error
+        /// </summary>
+        public static void CheckSyntaxError(this ICalReader reader, Func<bool> check, Func<string> message)
+        {
+            reader.CheckStrict(check, () => { throw new CalSyntaxError(message.Invoke()); });
+        }
+
+        /// <summary>
+        /// Make a default property parameter
+        /// </summary>
+        public static ICalPropertyParameter MakePropertyParameter(this ICalReader reader, string name, string value)
+        {
+            if (reader == null) return null;
+            var prm = reader.CreateDefaultParameter(name);
+            if (prm == null) return null;
+            if (prm.Deserialize(reader, name, value))
+                return prm;
+            return null;
+        }
+
+        /// <summary>
+        /// Make a typed property parameter
+        /// </summary>
+        public static T MakePropertyParameter<T>(this ICalReader reader, string name, string value) where T : class, ICalPropertyParameter, new()
+        {
+            var prm = new T();
+            if (prm.Deserialize(reader, name, value))
+                return prm;
+            return null;
+        }
+
+    }
+}
