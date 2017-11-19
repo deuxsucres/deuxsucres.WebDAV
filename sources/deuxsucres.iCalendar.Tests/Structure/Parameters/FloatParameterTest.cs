50fc184a91703349914736f9ba8935a42060e4a6
Yanos <ygrenier@ygrenier.com>
2017-11-19 15:32:27 +0100

    Implements float property parameter.

---
+++ sources/deuxsucres.iCalendar.Tests/Structure/Parameters/FloatParameterTest.cs
@@ -0,0 +1,92 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Structure.Parameters
+{
+    public class FloatParameterTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var param = new FloatParameter();
+            Assert.Equal(0, param.Value);
+        }
+
+        [Fact]
+        public void Cast()
+        {
+            var param = new FloatParameter();
+            Assert.Equal(0, param.Value);
+            Assert.Equal(0, (double)param);
+            param = 123.45;
+            Assert.Equal(123.45, param.Value);
+            Assert.Equal(123.45, (double)param);
+
+            double? iVal = null;
+            param = iVal;
+            Assert.Null(param);
+
+            iVal = 21.34;
+            param = iVal;
+            Assert.Equal(21.34, param.Value);
+
+            iVal = 23.45;
+            param = iVal;
+            Assert.Equal(23.45, param.Value);
+
+            param = null;
+            iVal = param;
+            Assert.Null(iVal);
+            Assert.Equal(0, (double)param);
+
+            iVal = new FloatParameter { Value = 98.76 };
+            Assert.Equal(98.76, iVal);
+
+            iVal = new FloatParameter { Value = 89.76 };
+            Assert.Equal(89.76, iVal);
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            var parser = new CalendarParser();
+            var mWriter = new Mock<ICalWriter>();
+            mWriter.SetupGet(w => w.Parser).Returns(parser);
+            var writer = mWriter.Object;
+            var mReader = new Mock<ICalReader>();
+            mReader.SetupGet(r => r.Parser).Returns(parser);
+            var reader = mReader.Object;
+
+            var param = new FloatParameter { Name = "Test", Value = 123.45 };
+            ContentLine line = new ContentLine
+            {
+                Name = "Line",
+                Value = "Content"
+            };
+            Assert.True(param.Serialize(writer, line));
+            Assert.Equal("Line;TEST=123.45:Content", writer.Parser.EncodeContentLine(line));
+            Assert.True(param.Deserialize(reader, "param", "32.45"));
+            Assert.Equal(32.45, param.Value);
+            Assert.False(param.Deserialize(reader, "param", "Test"));
+            Assert.Equal(0, param.Value);
+
+            param = new FloatParameter { Name = "Test", Value = 321.45 };
+            line = new ContentLine
+            {
+                Name = "Line",
+                Value = "Content"
+            };
+            Assert.True(param.Serialize(writer, line));
+            Assert.Equal("Line;TEST=321.45:Content", writer.Parser.EncodeContentLine(line));
+            
+        }
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Structure/Parameters/FloatParameter.cs
@@ -0,0 +1,60 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar.Structure
+{
+    /// <summary>
+    /// Float parameter
+    /// </summary>
+    public class FloatParameter : CalPropertyParameter
+    {
+
+        /// <summary>
+        /// Deserialize the value
+        /// </summary>
+        protected override string SerializeValue(ICalWriter writer, ContentLine line)
+        {
+            return writer.Parser.EncodeFloat(Value);
+        }
+
+        /// <summary>
+        /// Deserialize the parameter
+        /// </summary>
+        protected override bool InternalDeserialize(ICalReader reader, string name, string value)
+        {
+            Value = 0;
+            var b = reader.Parser.ParseFloat(value);
+            if (b == null) return false;
+            Value = b.Value;
+            return true;
+        }
+
+        /// <summary>
+        /// Cast to bool
+        /// </summary>
+        public static implicit operator double? (FloatParameter prop) { return prop?.Value; }
+
+        /// <summary>
+        /// Cast from bool
+        /// </summary>
+        public static implicit operator FloatParameter(double? value) { return value != null ? new FloatParameter { Value = value.Value } : null; }
+
+        /// <summary>
+        /// Cast to bool
+        /// </summary>
+        public static implicit operator double(FloatParameter prop) { return prop?.Value ?? 0; }
+
+        /// <summary>
+        /// Cast from bool
+        /// </summary>
+        public static implicit operator FloatParameter(double value) { return new FloatParameter { Value = value }; }
+
+        /// <summary>
+        /// Float value
+        /// </summary>
+        public double Value { get; set; }
+    }
+}
