8913f5fbc75f1f915862eca402afc901281f8500
Yanos <ygrenier@ygrenier.com>
2017-11-19 14:54:06 +0100

    Implements CalProperties.

---
+++ sources/deuxsucres.iCalendar.Tests/Structure/CalPropertiesTest.cs
@@ -0,0 +1,63 @@
+﻿using deuxsucres.iCalendar.Structure;
+using Moq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Structure
+{
+    public class CalPropertiesTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var mObject = new Mock<CalObject>() { CallBase = true };
+            var obj= mObject.Object;
+
+            var props = new CalProperties<CalProperty<int>>("test", obj);
+            Assert.Equal(0, props.Count);
+
+            Assert.Throws<ArgumentNullException>(() => new CalProperties<CalProperty<int>>("test", null));
+        }
+
+        [Fact]
+        public void Properties()
+        {
+            var mObject = new Mock<CalObject>() { CallBase = true };
+            var obj = mObject.Object;
+
+            var props = new CalProperties<CalProperty<int>>("test", obj);
+            Assert.Equal(0, props.Count);
+
+            var p1 = new CalProperty<int> { Value = 123 };
+            var p2 = new CalProperty<string> { Name = "test", Value = "456" };
+            var p3 = new CalProperty<int> { Name = "test", Value = 789 };
+
+            props.Add(p1);
+            Assert.Equal("test", p1.Name);
+            obj.AddProperty(p2);
+            obj.AddProperty(p3);
+            var p4 = obj.GetProperty("other", () => new CalProperty<int> { Value = 222 });
+
+            Assert.Equal(4, obj.PropertyCount);
+            Assert.Equal(2, props.Count);
+            Assert.Same(p1, props[0]);
+            Assert.Same(p3, props[1]);
+
+            Assert.Equal(new ICalProperty[] { p1, p3 }, props.ToArray());
+            Assert.Equal(new ICalProperty[] { p1, p2, p3, p4 }, obj.GetProperties());
+            props.Clear();
+            Assert.Equal(1, obj.PropertyCount);
+            Assert.Equal(0, props.Count);
+            Assert.Equal(new ICalProperty[] { }, props.ToArray());
+            Assert.Equal(new ICalProperty[] { p4 }, obj.GetProperties());
+
+            Assert.NotNull(((IEnumerable)props).GetEnumerator());
+        }
+
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Structure/CalProperties.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace deuxsucres.iCalendar.Structure
+{
+
+    /// <summary>
+    /// Wrapper for list of properties in a object
+    /// </summary>
+    public class CalProperties<T> : IEnumerable<T> where T : ICalProperty
+    {
+        CalObject _source;
+
+        /// <summary>
+        /// Create a new collection
+        /// </summary>
+        public CalProperties(string name, CalObject source)
+        {
+            _source = source ?? throw new ArgumentNullException(nameof(source));
+            Name = name;
+        }
+
+        /// <summary>
+        /// Remove all properties of this name
+        /// </summary>
+        public void Clear()
+        {
+            _source.RemoveProperty(Name);
+        }
+
+        /// <summary>
+        /// Add a property
+        /// </summary>
+        public void Add(T property)
+        {
+            property.Name = Name;
+            _source.AddProperty(property);
+        }
+
+        /// <summary>
+        /// Enumerate the components
+        /// </summary>
+        protected IEnumerable<T> GetProperties()
+        {
+            return _source.FindProperties<T>(Name);
+        }
+
+        /// <summary>
+        /// Get enumerator
+        /// </summary>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return GetProperties().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// Name of the property
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Get the component at a position
+        /// </summary>
+        public T this[int idx] { get { return GetProperties().ElementAt(idx); } }
+
+        /// <summary>
+        /// Count the properties
+        /// </summary>
+        public int Count => GetProperties().Count();
+
+    }
+
+}
