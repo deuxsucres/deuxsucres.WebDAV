3ddd872392255d3cfa240430ad33af2909255c5b
Yanos <ygrenier@ygrenier.com>
2017-11-19 15:51:53 +0100

    Implements uri property parameter.

---
+++ sources/deuxsucres.iCalendar.Tests/Structure/Parameters/UriParameterTest.cs
@@ -0,0 +1,70 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Structure.Parameters
+{
+    public class UriParameterTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var param = new UriParameter();
+            Assert.Null(param.Value);
+        }
+
+        [Fact]
+        public void Cast()
+        {
+            var param = new UriParameter();
+            Assert.Null(param.Value);
+            Assert.Null((Uri)param);
+            Assert.Null((string)param);
+
+            param = "http://www.deuxsucres.com";
+            Assert.Equal(new Uri("http://www.deuxsucres.com"), param.Value);
+            Assert.Equal(new Uri("http://www.deuxsucres.com"), (Uri)param);
+            Assert.Equal("http://www.deuxsucres.com/", (string)param);
+
+            param = new Uri("http://www.neige-informatique.fr");
+            Assert.Equal(new Uri("http://www.neige-informatique.fr"), param.Value);
+            Assert.Equal(new Uri("http://www.neige-informatique.fr"), (Uri)param);
+            Assert.Equal("http://www.neige-informatique.fr/", (string)param);
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            var parser = new CalendarParser();
+            var mWriter = new Mock<ICalWriter>();
+            mWriter.SetupGet(w => w.Parser).Returns(parser);
+            var writer = mWriter.Object;
+            var mReader = new Mock<ICalReader>();
+            mReader.SetupGet(r => r.Parser).Returns(parser);
+            var reader = mReader.Object;
+
+            var param = new UriParameter { Name = "Test", Value = new Uri("http://www.deuxsucres.com") };
+            ContentLine line = new ContentLine
+            {
+                Name = "Line",
+                Value = "Content"
+            };
+            Assert.True(param.Serialize(writer, line));
+            Assert.Equal("Line;TEST=\"http://www.deuxsucres.com/\":Content", writer.Parser.EncodeContentLine(line));
+            Assert.True(param.Deserialize(reader, "param", "\"http://www.neige-informatique.fr\""));
+            Assert.Equal(new Uri("http://www.neige-informatique.fr"), param.Value);
+            Assert.False(param.Deserialize(reader, "param", "\"Test\""));
+            Assert.Null(param.Value);
+            Assert.False(param.Deserialize(reader, "param", "Test"));
+            Assert.Null(param.Value);
+        }
+
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Structure/Parameters/UriParameter.cs
@@ -0,0 +1,59 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar.Structure
+{
+
+    /// <summary>
+    /// Uri parameter
+    /// </summary>
+    public class UriParameter : CalPropertyParameter
+    {
+
+        /// <summary>
+        /// Serialize the value
+        /// </summary>
+        protected override string SerializeValue(ICalWriter writer, ContentLine line)
+        {
+            return writer.Parser.EncodeUri(Value);
+        }
+
+        /// <summary>
+        /// Deserialize the parameter
+        /// </summary>
+        protected override bool InternalDeserialize(ICalReader reader, string name, string value)
+        {
+            Value = reader.Parser.ParseUri(value, true);
+            return Value != null;
+        }
+
+        /// <summary>
+        /// Cast to Uri
+        /// </summary>
+        public static implicit operator Uri(UriParameter param) => param?.Value;
+
+        /// <summary>
+        /// Cast from Uri
+        /// </summary>
+        public static implicit operator UriParameter(Uri uri) => uri != null ? new UriParameter { Value = uri } : null;
+
+        /// <summary>
+        /// Cast to string
+        /// </summary>
+        public static implicit operator string(UriParameter param) => param?.Value?.ToString();
+
+        /// <summary>
+        /// Cast from string
+        /// </summary>
+        public static implicit operator UriParameter(string str) => str != null ? new UriParameter { Value = new Uri(str) } : null;
+
+        /// <summary>
+        /// Uri value
+        /// </summary>
+        public Uri Value { get; set; }
+    }
+
+}
