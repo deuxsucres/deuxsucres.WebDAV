b5e8ea1b17374a778b8fe87477a680ce82377ac4
Yanos <ygrenier@ygrenier.com>
2017-11-19 15:40:27 +0100

    Implements text property parameter.

---
+++ sources/deuxsucres.iCalendar.Tests/Structure/Parameters/TextParameterTest.cs
@@ -0,0 +1,81 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Structure.Parameters
+{
+    public class TextParameterTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var param = new TextParameter();
+            Assert.Null(param.Value);
+        }
+
+        [Fact]
+        public void Cast()
+        {
+            var param = new TextParameter();
+            Assert.Null(param.Value);
+            Assert.Null((string)param);
+            param = "Test";
+            Assert.Equal("Test", param.Value);
+            Assert.Equal("Test", (string)param);
+
+            string sVal = null;
+            param = sVal;
+            Assert.Null(param);
+
+            sVal = "Toto";
+            param = sVal;
+            Assert.Equal("Toto", param.Value);
+
+            param = null;
+            sVal = param;
+            Assert.Null(sVal);
+            Assert.Null((string)param);
+
+            sVal = new TextParameter { Value = "Text" };
+            Assert.Equal("Text", sVal);
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            var parser = new CalendarParser();
+            var mWriter = new Mock<ICalWriter>();
+            mWriter.SetupGet(w => w.Parser).Returns(parser);
+            var writer = mWriter.Object;
+            var mReader = new Mock<ICalReader>();
+            mReader.SetupGet(r => r.Parser).Returns(parser);
+            var reader = mReader.Object;
+
+            var param = new TextParameter { Name = "Test", Value = "Test" };
+            ContentLine line = new ContentLine
+            {
+                Name = "Line",
+                Value = "Content"
+            };
+            Assert.True(param.Serialize(writer, line));
+            Assert.Equal("Line;TEST=Test:Content", writer.Parser.EncodeContentLine(line));
+            Assert.True(param.Deserialize(reader, "param", "Other"));
+            Assert.Equal("Other", param.Value);
+            
+        }
+
+        [Fact]
+        public void TestToString()
+        {
+            var param = new TextParameter { Name = "Test", Value = "Value" };
+            Assert.Equal("Value", param.ToString());
+        }
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Structure/Parameters/TextParameter.cs
@@ -0,0 +1,55 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar.Structure
+{
+    /// <summary>
+    /// Text parameter
+    /// </summary>
+    public class TextParameter : CalPropertyParameter
+    {
+
+        /// <summary>
+        /// Serialize the value
+        /// </summary>
+        protected override string SerializeValue(ICalWriter writer, ContentLine line)
+        {
+            return writer.Parser.EncodeTextParameter(Value);
+        }
+
+        /// <summary>
+        /// Deserialize the parameter
+        /// </summary>
+        protected override bool InternalDeserialize(ICalReader reader, string name, string value)
+        {
+            Value = reader.Parser.ParseText(value);
+            return Value != null;
+        }
+
+        /// <summary>
+        /// To string
+        /// </summary>
+        public override string ToString()
+        {
+            return Value;
+        }
+
+        /// <summary>
+        /// Cast vers un string
+        /// </summary>
+        public static implicit operator string(TextParameter prop) { return prop?.Value; }
+
+        /// <summary>
+        /// Cast depuis un string
+        /// </summary>
+        public static implicit operator TextParameter(string value) { return value != null ? new TextParameter { Value = value } : null; }
+
+        /// <summary>
+        /// Text value
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
