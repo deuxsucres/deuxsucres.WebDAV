846c4e3f25472555e702404ea0d759e83c706743
Yanos <ygrenier@ygrenier.com>
2017-11-19 15:27:06 +0100

    Implements int property parameter.

---
+++ sources/deuxsucres.iCalendar.Tests/Structure/Parameters/IntParameterTest.cs
@@ -0,0 +1,92 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Structure.Parameters
+{
+    public class IntParameterTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var param = new IntParameter();
+            Assert.Equal(0, param.Value);
+        }
+
+        [Fact]
+        public void Cast()
+        {
+            var param = new IntParameter();
+            Assert.Equal(0, param.Value);
+            Assert.Equal(0, (int)param);
+            param = 123;
+            Assert.Equal(123, param.Value);
+            Assert.Equal(123, (int)param);
+
+            int? iVal = null;
+            param = iVal;
+            Assert.Null(param);
+
+            iVal = 21;
+            param = iVal;
+            Assert.Equal(21, param.Value);
+
+            iVal = 23;
+            param = iVal;
+            Assert.Equal(23, param.Value);
+
+            param = null;
+            iVal = param;
+            Assert.Null(iVal);
+            Assert.Equal(0, (int)param);
+
+            iVal = new IntParameter { Value = 98 };
+            Assert.Equal(98, iVal);
+
+            iVal = new IntParameter { Value = 89 };
+            Assert.Equal(89, iVal);
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            var parser = new CalendarParser();
+            var mWriter = new Mock<ICalWriter>();
+            mWriter.SetupGet(w => w.Parser).Returns(parser);
+            var writer = mWriter.Object;
+            var mReader = new Mock<ICalReader>();
+            mReader.SetupGet(r => r.Parser).Returns(parser);
+            var reader = mReader.Object;
+
+            var param = new IntParameter { Name = "Test", Value = 123 };
+            ContentLine line = new ContentLine
+            {
+                Name = "Line",
+                Value = "Content"
+            };
+            Assert.True(param.Serialize(writer, line));
+            Assert.Equal("Line;TEST=123:Content", writer.Parser.EncodeContentLine(line));
+            Assert.True(param.Deserialize(reader, "param", "32"));
+            Assert.Equal(32, param.Value);
+            Assert.False(param.Deserialize(reader, "param", "Test"));
+            Assert.Equal(0, param.Value);
+
+            param = new IntParameter { Name = "Test", Value = 321 };
+            line = new ContentLine
+            {
+                Name = "Line",
+                Value = "Content"
+            };
+            Assert.True(param.Serialize(writer, line));
+            Assert.Equal("Line;TEST=321:Content", writer.Parser.EncodeContentLine(line));
+            
+        }
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Structure/Parameters/IntParameter.cs
@@ -0,0 +1,60 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar.Structure
+{
+    /// <summary>
+    /// Integer parameter
+    /// </summary>
+    public class IntParameter : CalPropertyParameter
+    {
+
+        /// <summary>
+        /// Deserialize the value
+        /// </summary>
+        protected override string SerializeValue(ICalWriter writer, ContentLine line)
+        {
+            return writer.Parser.EncodeInt(Value);
+        }
+
+        /// <summary>
+        /// Deserialize the parameter
+        /// </summary>
+        protected override bool InternalDeserialize(ICalReader reader, string name, string value)
+        {
+            Value = 0;
+            var b = reader.Parser.ParseInt(value);
+            if (b == null) return false;
+            Value = b.Value;
+            return true;
+        }
+
+        /// <summary>
+        /// Cast to bool
+        /// </summary>
+        public static implicit operator int? (IntParameter prop) { return prop?.Value; }
+
+        /// <summary>
+        /// Cast from bool
+        /// </summary>
+        public static implicit operator IntParameter(int? value) { return value != null ? new IntParameter { Value = value.Value } : null; }
+
+        /// <summary>
+        /// Cast to bool
+        /// </summary>
+        public static implicit operator int(IntParameter prop) { return prop?.Value ?? 0; }
+
+        /// <summary>
+        /// Cast from bool
+        /// </summary>
+        public static implicit operator IntParameter(int value) { return new IntParameter { Value = value }; }
+
+        /// <summary>
+        /// Integer value
+        /// </summary>
+        public int Value { get; set; }
+    }
+}
