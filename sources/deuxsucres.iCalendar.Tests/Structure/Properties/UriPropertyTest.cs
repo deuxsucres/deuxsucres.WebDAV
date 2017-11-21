92c7eb4266b4094e4a4a1a9c1c282e8f088f937e
Yanos <ygrenier@ygrenier.com>
2017-11-21 20:19:57 +0100

    Implements URI property.

---
+++ sources/deuxsucres.iCalendar.Tests/Structure/Properties/UriPropertyTest.cs
@@ -0,0 +1,83 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Structure.Parameters
+{
+    public class UriPropertyTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var prop = new UriProperty();
+            Assert.Null(prop.Value);
+        }
+
+        [Fact]
+        public void Cast()
+        {
+            var prop = new UriProperty();
+            Assert.Null(prop.Value);
+            Assert.Null((Uri)prop);
+            Assert.Null((string)prop);
+
+            prop = "http://www.deuxsucres.com";
+            Assert.Equal(new Uri("http://www.deuxsucres.com"), prop.Value);
+            Assert.Equal(new Uri("http://www.deuxsucres.com"), (Uri)prop);
+            Assert.Equal("http://www.deuxsucres.com/", (string)prop);
+
+            prop = new Uri("http://www.neige-informatique.fr");
+            Assert.Equal(new Uri("http://www.neige-informatique.fr"), prop.Value);
+            Assert.Equal(new Uri("http://www.neige-informatique.fr"), (Uri)prop);
+            Assert.Equal("http://www.neige-informatique.fr/", (string)prop);
+        }
+
+        [Fact]
+        public void TestToString()
+        {
+            UriProperty prop = "http://www.deuxsucres.com";
+            Assert.Equal("http://www.deuxsucres.com/", prop.ToString());
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            var parser = new CalendarParser();
+            var mWriter = new Mock<ICalWriter>();
+            mWriter.SetupGet(w => w.Parser).Returns(parser);
+            var writer = mWriter.Object;
+            var mReader = new Mock<ICalReader>();
+            mReader.SetupGet(r => r.Parser).Returns(parser);
+            var reader = mReader.Object;
+
+            var prop = new UriProperty { Name = "Test", Value = new Uri("http://www.deuxsucres.com") };
+            ContentLine line;
+            Assert.NotNull(line = prop.Serialize(writer));
+            Assert.Equal("TEST:http://www.deuxsucres.com/", writer.Parser.EncodeContentLine(line));
+
+            line = new ContentLine { Name = "param", Value = "http://www.neige-informatique.fr" };
+            prop.Deserialize(reader, line);
+            Assert.Equal(new Uri("http://www.neige-informatique.fr"), prop.Value);
+
+            line = new ContentLine { Name = "param", Value = "\"http://www.neige-informatique.fr\"" };
+            prop.Deserialize(reader, line);
+            Assert.Equal(new Uri("http://www.neige-informatique.fr"), prop.Value);
+
+            line = new ContentLine { Name = "param", Value = "\"Test\"" };
+            prop.Deserialize(reader, line);
+            Assert.Null(prop.Value);
+
+            line = new ContentLine { Name = "param", Value = "Test" };
+            prop.Deserialize(reader, line);
+            Assert.Null(prop.Value);
+        }
+
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Structure/Properties/UriProperty.cs
@@ -0,0 +1,70 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar.Structure
+{
+    /// <summary>
+    /// URI value property
+    /// </summary>
+    public class UriProperty : CalProperty
+    {
+
+        #region Serialization
+
+        /// <summary>
+        /// Serialize a value
+        /// </summary>
+        protected override string SerializeValue(ICalWriter writer, ContentLine line)
+        {
+            return writer.Parser.EncodeUri(Value, false);
+        }
+
+        /// <summary>
+        /// Deserialize the value
+        /// </summary>
+        protected override bool DeserializeValue(ICalReader reader, ContentLine line)
+        {
+            Value = reader.Parser.ParseUri(line.Value, false);
+            return Value != null;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// To string
+        /// </summary>
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+        /// <summary>
+        /// Cast to Uri
+        /// </summary>
+        public static implicit operator Uri(UriProperty prop) { return prop?.Value; }
+
+        /// <summary>
+        /// Cast from Uri
+        /// </summary>
+        public static implicit operator UriProperty(Uri value) { return value != null ? new UriProperty { Value = value } : null; }
+
+        /// <summary>
+        /// Cast to String
+        /// </summary>
+        public static implicit operator String(UriProperty prop) { return prop?.Value?.ToString(); }
+
+        /// <summary>
+        /// Cast from String
+        /// </summary>
+        public static implicit operator UriProperty(string value) { return value != null ? new UriProperty { Value = new Uri(value) } : null; }
+
+        /// <summary>
+        /// Uri value
+        /// </summary>
+        public Uri Value { get; set; }
+
+    }
+}
