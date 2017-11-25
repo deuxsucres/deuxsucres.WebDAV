19f3d60dce92c3df9fece971bbde51618d59acd7
Yanos <ygrenier@ygrenier.com>
2017-11-25 15:55:09 +0100

    Implements the typed components list.

---
+++ sources/deuxsucres.iCalendar.Tests/Structure/CalComponentsTest.cs
@@ -0,0 +1,72 @@
+﻿using deuxsucres.iCalendar.Structure;
+using Moq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Structure
+{
+    public class CalComponentsTest
+    {
+        class TestComponent : CalComponent
+        {
+            public override string Name => "Test";
+        }
+
+        [Fact]
+        public void Create()
+        {
+            var source = new List<CalComponent>();
+            var comps = new CalComponents<TestComponent>(source);
+            Assert.Equal(0, comps.Count);
+
+            Assert.Throws<ArgumentNullException>(() => new CalComponents<TestComponent>(null));
+        }
+
+        [Fact]
+        public void Components()
+        {
+            var c1 = new TestComponent();
+            var c2 = new Mock<CalComponent>() { CallBase = true }.Object;
+            var c3 = new TestComponent();
+            var c4 = new TestComponent();
+
+            var source = new List<CalComponent> { c1, c2, c3 };
+
+            var comps = new CalComponents<TestComponent>(source);
+            Assert.Equal(2, comps.Count);
+
+            comps.Add(c4);
+            Assert.Equal(3, comps.Count);
+            comps.Add(c4);
+            Assert.Equal(3, comps.Count);
+
+            var c5 = comps.CreateNew();
+            Assert.Equal(4, comps.Count);
+
+            Assert.Equal(new CalComponent[] { c1, c2, c3, c4, c5 }, source);
+            Assert.Equal(new CalComponent[] { c1, c3, c4, c5 }, comps);
+
+            Assert.Same(c1, comps[0]);
+            Assert.Same(c3, comps[1]);
+            Assert.Same(c4, comps[2]);
+
+            Assert.True(comps.Remove(c4));
+            Assert.False(comps.Remove(c4));
+
+            Assert.Equal(new CalComponent[] { c1, c2, c3, c5 }, source);
+            Assert.Equal(new CalComponent[] { c1, c3, c5 }, comps);
+
+            comps.Clear();
+            Assert.Equal(new CalComponent[] { c2}, source);
+            Assert.Equal(new CalComponent[] { }, comps);
+
+            Assert.NotNull(((IEnumerable)comps).GetEnumerator());
+        }
+
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Structure/CalComponents.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace deuxsucres.iCalendar.Structure
+{
+    /// <summary>
+    /// Wrapper for list of components in a object
+    /// </summary>
+    public class CalComponents<T> : IEnumerable<T> where T : CalComponent
+    {
+        IList<CalComponent> _source;
+
+        /// <summary>
+        /// Create a new list
+        /// </summary>
+        public CalComponents(IList<CalComponent> source)
+        {
+            _source = source ?? throw new ArgumentNullException(nameof(source));
+        }
+
+        /// <summary>
+        /// Enumerate the components
+        /// </summary>
+        protected IEnumerable<T> GetComponents()
+        {
+            return _source.OfType<T>();
+        }
+
+        /// <summary>
+        /// Get enumerator
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return GetComponents().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        /// <summary>
+        /// Remove all components of this list
+        /// </summary>
+        public void Clear()
+        {
+            var comps = this.ToList();
+            foreach (var comp in comps)
+                _source.Remove(comp);
+        }
+
+        /// <summary>
+        /// Add a component
+        /// </summary>
+        public void Add(T component)
+        {
+            if (component != null && !_source.Contains(component))
+                _source.Add(component);
+        }
+
+        /// <summary>
+        /// Create and add a new component
+        /// </summary>
+        public T CreateNew()
+        {
+            var comp = (T)Activator.CreateInstance(typeof(T));
+            _source.Add(comp);
+            return comp;
+        }
+
+        /// <summary>
+        /// Remove a component
+        /// </summary>
+        public bool Remove(T component)
+        {
+            return _source.Remove(component);
+        }
+
+        /// <summary>
+        /// Get the component at a position
+        /// </summary>
+        public T this[int idx] { get { return GetComponents().ElementAt(idx); } }
+
+        /// <summary>
+        /// Count the components
+        /// </summary>
+        public int Count => GetComponents().Count();
+
+    }
+}
