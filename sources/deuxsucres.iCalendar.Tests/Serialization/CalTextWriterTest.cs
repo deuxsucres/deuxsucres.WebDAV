d6dc9b314fe57c8049363a4f1f7700c12324c002
Yanos <ygrenier@ygrenier.com>
2017-11-25 16:15:45 +0100

    Implements a calendar writer to TextWriter.

---
+++ sources/deuxsucres.iCalendar.Tests/Serialization/CalTextWriterTest.cs
@@ -0,0 +1,49 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Serialization
+{
+    public class CalTextWriterTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var parser = new CalendarParser();
+            var twriter = new StringWriter();
+            var writer = new CalTextWriter(parser, twriter);
+            Assert.Same(parser, writer.Parser);
+            Assert.Same(twriter, writer.Source);
+
+            Assert.Throws<ArgumentNullException>(() => new CalTextWriter(null, twriter));
+            Assert.Throws<ArgumentNullException>(() => new CalTextWriter(parser, null));
+        }
+
+        [Fact]
+        public void Write()
+        {
+            var parser = new CalendarParser();
+            var output = new StringBuilder();
+            using (var twriter = new StringWriter(output))
+            {
+                var writer = new CalTextWriter(parser, twriter);
+                writer.WriteBegin("Test");
+                var content = new ContentLine { Name = "Prop", Value = "Value" };
+                content.SetParam("P1", "1").SetParam("p2", "2");
+                writer.WriteLine(content);
+                writer.WriteEnd("Test");
+            }
+            Assert.Equal(new StringBuilder()
+                .AppendLine("BEGIN:TEST")
+                .AppendLine("Prop;P1=1;p2=2:Value")
+                .AppendLine("END:TEST")
+                .ToString(), output.ToString());
+        }
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Serialization/CalTextWriter.cs
@@ -0,0 +1,75 @@
+﻿using deuxsucres.iCalendar.Parser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace deuxsucres.iCalendar.Serialization
+{
+    /// <summary>
+    /// Calendar writer to a TextWriter
+    /// </summary>
+    public class CalTextWriter : ICalWriter
+    {
+        
+        /// <summary>
+        /// Create a ne wwriter
+        /// </summary>
+        public CalTextWriter(CalendarParser parser, TextWriter source)
+        {
+            Parser = parser ?? throw new ArgumentNullException(nameof(parser));
+            Source = source ?? throw new ArgumentNullException(nameof(source));
+            CurrentLineNumber = 1;
+        }
+
+        /// <summary>
+        /// Write a line
+        /// </summary>
+        protected virtual void WriteLine(string line)
+        {
+            CurrentLineNumber += Parser.WriteLine(Source, line);
+        }
+
+        /// <summary>
+        /// Write a BEGIN line
+        /// </summary>
+        public ICalWriter WriteBegin(string value)
+        {
+            WriteLine($"{Constants.BEGIN}:{value.ToUpper()}");
+            return this;
+        }
+
+        /// <summary>
+        /// Write a END line
+        /// </summary>
+        public ICalWriter WriteEnd(string value)
+        {
+            WriteLine($"{Constants.END}:{value.ToUpper()}");
+            return this;
+        }
+
+        /// <summary>
+        /// Write a content line
+        /// </summary>
+        public ICalWriter WriteLine(ContentLine line)
+        {
+            CurrentLineNumber += Parser.WriteContentLine(Source, line);
+            return this;
+        }
+
+        /// <summary>
+        /// Parser
+        /// </summary>
+        public CalendarParser Parser { get; private set; }
+
+        /// <summary>
+        /// Source
+        /// </summary>
+        public TextWriter Source { get; private set; }
+
+        /// <summary>
+        /// Current number line
+        /// </summary>
+        public int CurrentLineNumber { get; private set; }
+    }
+}
