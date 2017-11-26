de0423bbec2a6c21fb3319a5348e97314d0e2c96
Yanos <ygrenier@ygrenier.com>
2017-11-26 18:49:13 +0100

    Implements UTC-OFFSET property.

---
+++ sources/deuxsucres.iCalendar.Tests/Structure/Properties/UtcOffsetPropertyTest.cs
@@ -0,0 +1,90 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Structure.Properties
+{
+    public class UtcOffsetPropertyTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var prop = new UtcOffsetProperty();
+            Assert.Equal(TimeSpan.Zero, prop.Value);
+        }
+
+        [Fact]
+        public void Cast()
+        {
+            var prop = new UtcOffsetProperty();
+            Assert.Equal(TimeSpan.Zero, prop.Value);
+            Assert.Equal(TimeSpan.Zero, (TimeSpan)prop);
+            Assert.Equal(TimeSpan.Zero, (TimeSpan?)prop);
+
+            TimeSpan ts = TimeSpan.FromMinutes(123);
+
+            prop = ts;
+            Assert.Equal(ts, prop.Value);
+            Assert.Equal(ts, (TimeSpan)prop);
+            Assert.Equal(ts, (TimeSpan?)prop);
+
+            TimeSpan? tsn = ts;
+            prop = tsn;
+            Assert.Equal(ts, prop.Value);
+            Assert.Equal(ts, (TimeSpan)prop);
+            Assert.Equal(ts, (TimeSpan?)prop);
+
+            tsn = null;
+            prop = tsn;
+            Assert.Null(prop);
+            Assert.Equal(TimeSpan.Zero, (TimeSpan)prop);
+            Assert.Null((TimeSpan?)prop);
+        }
+
+        [Fact]
+        public void TestToString()
+        {
+            TimeSpan ts = TimeSpan.FromMinutes(123);
+            UtcOffsetProperty prop = ts;
+            Assert.Equal(ts.ToString(), prop.ToString());
+            Assert.Equal(ts.ToString("c"), prop.ToString("c"));
+            Assert.Equal(ts.ToString("c", CultureInfo.InvariantCulture), prop.ToString("c", CultureInfo.InvariantCulture));
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            var parser = new CalendarParser();
+            var mWriter = new Mock<ICalWriter>();
+            mWriter.SetupGet(w => w.Parser).Returns(parser);
+            var writer = mWriter.Object;
+            var mReader = new Mock<ICalReader>();
+            mReader.SetupGet(r => r.Parser).Returns(parser);
+            var reader = mReader.Object;
+
+            TimeSpan ts = TimeSpan.FromMinutes(123);
+            UtcOffsetProperty prop = new UtcOffsetProperty { Name = "Test", Value = ts };
+            ContentLine line = prop.Serialize(writer);
+            Assert.NotNull(line);
+            Assert.Equal("TEST:+0203", writer.Parser.EncodeContentLine(line));
+
+            line = new ContentLine { Name = "prop", Value = "+0203" };
+            prop.Deserialize(reader, line);
+            Assert.Equal("prop", prop.Name);
+            Assert.Equal(ts, prop.Value);
+
+            line = new ContentLine { Name = "test", Value = "test" };
+            prop.Deserialize(reader, line);
+            Assert.Equal("test", prop.Name);
+            Assert.Equal(TimeSpan.Zero, prop.Value);
+        }
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Structure/Properties/UtcOffsetProperty.cs
@@ -0,0 +1,97 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar.Structure
+{
+    /// <summary>
+    /// UTC-OFFSET
+    /// </summary>
+    public class UtcOffsetProperty : CalProperty, IFormattable
+    {
+
+        /// <summary>
+        /// Reset
+        /// </summary>
+        public override void Reset()
+        {
+            base.Reset();
+            Value = TimeSpan.Zero;
+        }
+
+        #region Serialization
+
+        /// <summary>
+        /// Serialize the value
+        /// </summary>
+        protected override string SerializeValue(ICalWriter writer, ContentLine line)
+        {
+            return writer.Parser.EncodeUtcOffset(Value);
+        }
+
+        /// <summary>
+        /// Deserialize the value
+        /// </summary>
+        protected override bool DeserializeValue(ICalReader reader, ContentLine line)
+        {
+            var du = reader.Parser.ParseUtcOffset(line.Value);
+            if (!du.HasValue) return false;
+            Value = du.Value;
+            return true;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// To string
+        /// </summary>
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+        /// <summary>
+        /// To string
+        /// </summary>
+        public virtual string ToString(string format)
+        {
+            return Value.ToString(format);
+        }
+
+        /// <summary>
+        /// To string
+        /// </summary>
+        public virtual string ToString(string format, IFormatProvider formatProvider)
+        {
+            return Value.ToString(format, formatProvider);
+        }
+
+        /// <summary>
+        /// Cast to TimeSpan
+        /// </summary>
+        public static implicit operator TimeSpan? (UtcOffsetProperty prop) { return prop?.Value; }
+
+        /// <summary>
+        /// Cast from TimeSpan
+        /// </summary>
+        public static implicit operator UtcOffsetProperty(TimeSpan? value) { return value != null ? new UtcOffsetProperty { Value = value.Value } : null; }
+
+        /// <summary>
+        /// Cast to TimeSpan
+        /// </summary>
+        public static implicit operator TimeSpan(UtcOffsetProperty prop) { return prop?.Value ?? TimeSpan.Zero; }
+
+        /// <summary>
+        /// Cast from TimeSpan
+        /// </summary>
+        public static implicit operator UtcOffsetProperty(TimeSpan value) { return new UtcOffsetProperty { Value = value }; }
+
+        /// <summary>
+        /// TimeSpan value
+        /// </summary>
+        public virtual TimeSpan Value { get; set; }
+
+    }
+}
