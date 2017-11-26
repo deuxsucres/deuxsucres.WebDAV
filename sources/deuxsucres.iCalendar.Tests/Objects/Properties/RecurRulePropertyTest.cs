053debcd5452bf32e4ce889a2d3dbb8414587c4d
Yanos <ygrenier@ygrenier.com>
2017-11-26 10:48:54 +0100

    Implements RRULE property.

---
+++ sources/deuxsucres.iCalendar.Tests/Objects/Properties/RecurRulePropertyTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Objects.Properties
+{
+    public class RecurRulePropertyTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var prop = new RecurRuleProperty();
+            Assert.Equal("RRULE", prop.Name);
+
+            Recurrence rec = prop;
+            Assert.Null(rec);
+
+            prop.Value = new Recurrence { Count = 2 };
+            rec = prop;
+            Assert.NotNull(rec);
+            Assert.Equal(2, rec.Count);
+
+            rec = new Recurrence() { };
+            prop = rec;
+            Assert.Same(rec, prop.Value);
+
+            rec = null;
+            prop = rec;
+            Assert.Null(prop);
+        }
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Objects/Properties/RecurRuleProperty.cs
@@ -0,0 +1,31 @@
+﻿using deuxsucres.iCalendar.Structure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar
+{
+    /// <summary>
+    /// RRULE
+    /// </summary>
+    public class RecurRuleProperty : RecurrenceProperty
+    {
+        /// <summary>
+        /// New property
+        /// </summary>
+        public RecurRuleProperty()
+        {
+            Name = Constants.RRULE;
+        }
+
+        /// <summary>
+        /// Cast to recurrence
+        /// </summary>
+        public static implicit operator Recurrence(RecurRuleProperty prop) { return prop.Value; }
+
+        /// <summary>
+        /// Cast from recurrence
+        /// </summary>
+        public static implicit operator RecurRuleProperty(Recurrence value) { return value != null ? new RecurRuleProperty { Value = value } : null; }
+    }
+}
