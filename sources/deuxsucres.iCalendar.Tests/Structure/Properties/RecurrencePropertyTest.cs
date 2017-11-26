d42f74e9b68284b76b9d3a7ffdf97fe6f32f8e87
Yanos <ygrenier@ygrenier.com>
2017-11-26 08:26:32 +0100

    Implements the RECURRENCE property.

---
+++ sources/deuxsucres.iCalendar.Tests/Structure/Properties/RecurrencePropertyTest.cs
@@ -0,0 +1,77 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Structure.Properties
+{
+    public class RecurrencePropertyTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var prop = new RecurrenceProperty();
+            Assert.Null(prop.Value);
+        }
+
+        [Fact]
+        public void Cast()
+        {
+            var rec = new Recurrence();
+            var prop = new RecurrenceProperty();
+            Assert.Null(prop.Value);
+            Assert.Null((Recurrence)prop);
+            prop = rec;
+            Assert.Same(rec, prop.Value);
+            Assert.Same(rec, (Recurrence)prop);
+
+            rec = null;
+            prop = rec;
+            Assert.Null(prop);
+            Assert.Null((Recurrence)prop);
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            var parser = new CalendarParser();
+            var mWriter = new Mock<ICalWriter>();
+            mWriter.SetupGet(w => w.Parser).Returns(parser);
+            var writer = mWriter.Object;
+            var mReader = new Mock<ICalReader>();
+            mReader.SetupGet(r => r.Parser).Returns(parser);
+            var reader = mReader.Object;
+
+            var prop = new RecurrenceProperty { Name = "Test"};
+            ContentLine line = prop.Serialize(writer);
+            Assert.NotNull(line);
+            Assert.Equal("TEST:", writer.Parser.EncodeContentLine(line));
+
+            var rec = new Recurrence() { ByHour = new int[] { 5, 12 }, Count = 17, Frequency = Recurrence.Frequencies.Minutely };
+            prop.Value = rec;
+            line = prop.Serialize(writer);
+            Assert.NotNull(line);
+            Assert.Equal("TEST:FREQ=MINUTELY;COUNT=17;BYHOUR=5,12", writer.Parser.EncodeContentLine(line));
+
+            line = new ContentLine { Name = "Prop", Value = "FREQ=HOURLY;COUNT=2;BYHOUR=2,4,6" };
+            prop.Deserialize(reader, line);
+            Assert.Equal("Prop", prop.Name);
+            Assert.NotNull(prop.Value);
+            Assert.Equal(Recurrence.Frequencies.Hourly, prop.Value.Frequency);
+            Assert.Equal(2, prop.Value.Count);
+            Assert.Equal(new int[] { 2, 4, 6 }, prop.Value.ByHour);
+
+            line = new ContentLine { Name = "Recur", Value = "test" };
+            prop.Deserialize(reader, line);
+            Assert.Equal("Recur", prop.Name);
+            Assert.Null(prop.Value);
+        }
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Structure/Properties/RecurrenceProperty.cs
@@ -0,0 +1,51 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar.Structure
+{
+    /// <summary>
+    /// RECURRENCE property
+    /// </summary>
+    public class RecurrenceProperty : CalProperty
+    {
+        #region Serialization
+
+        /// <summary>
+        /// Serialize the value
+        /// </summary>
+        protected override string SerializeValue(ICalWriter writer, ContentLine line)
+        {
+            return writer.Parser.EncodeRecur(Value);
+        }
+
+        /// <summary>
+        /// Deserialize the value
+        /// </summary>
+        protected override bool DeserializeValue(ICalReader reader, ContentLine line)
+        {
+            Value = reader.Parser.ParseRecur(line.Value);
+            return Value != null;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Cast to recurrence
+        /// </summary>
+        public static implicit operator Recurrence(RecurrenceProperty prop) { return prop?.Value; }
+
+        /// <summary>
+        /// Cast from recurrence
+        /// </summary>
+        public static implicit operator RecurrenceProperty(Recurrence value) { return value != null ? new RecurrenceProperty { Value = value } : null; }
+
+        /// <summary>
+        /// Recurrence value
+        /// </summary>
+        public virtual Recurrence Value { get; set; }
+
+    }
+}
