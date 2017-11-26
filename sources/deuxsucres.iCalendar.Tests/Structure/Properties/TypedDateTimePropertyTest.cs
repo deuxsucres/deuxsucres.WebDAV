046b3482180983a9ce46b37e76045129a65bb6ff
Yanos <ygrenier@ygrenier.com>
2017-11-26 18:34:19 +0100

    Implements a typed datetime property (can be a DATE or a DATETIME).

---
+++ sources/deuxsucres.iCalendar.Tests/Structure/Properties/TypedDateTimePropertyTest.cs
@@ -0,0 +1,164 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Structure.Properties
+{
+    public class TypedDateTimePropertyTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var prop = new TypedDateTimeProperty();
+            Assert.Equal(DateTime.MinValue, prop.Value);
+            Assert.False(prop.IsDate);
+            Assert.Null(prop.TimeZoneID);
+        }
+
+        [Fact]
+        public void Cast()
+        {
+            var prop = new TypedDateTimeProperty();
+            DateTime dt = prop;
+            DateTime? dtn = prop;
+            Assert.Equal(DateTime.MinValue, dt);
+            Assert.Equal(DateTime.MinValue, dtn);
+
+            prop = null;
+            dt = prop;
+            dtn = prop;
+            Assert.Equal(DateTime.MinValue, dt);
+            Assert.Null(dtn);
+
+            DateTime now = DateTime.Now;
+            prop = new TypedDateTimeProperty { Value = now };
+            dt = prop;
+            dtn = prop;
+            Assert.Equal(now, dt);
+            Assert.Equal(now, dtn);
+
+            dt = now;
+            prop = dt;
+            Assert.Equal(dt, prop.Value);
+
+            dtn = now;
+            prop = dtn;
+            Assert.Equal(dt, prop.Value);
+
+            dtn = null;
+            prop = dtn;
+            Assert.Null(prop);
+        }
+
+        [Fact]
+        public void Properties()
+        {
+            DateTime now = DateTime.Now;
+            var prop = new TypedDateTimeProperty()
+            {
+                Value = now,
+                TimeZoneID = "TimeZone"
+            };
+            Assert.Equal(now, prop.Value);
+            Assert.Equal("TimeZone", prop.TimeZoneID);
+            Assert.False(prop.IsDate);
+
+            prop.Value = now.Date;
+            prop.TimeZoneID = null;
+            Assert.Equal(now.Date, prop.Value);
+            Assert.Null(prop.TimeZoneID);
+            Assert.True(prop.IsDate);
+        }
+
+        [Fact]
+        public void SetAs()
+        {
+            DateTime now = DateTime.Now;
+            var prop = new TypedDateTimeProperty();
+            prop.SetAsDateTime(now);
+            Assert.Equal(now, prop.Value);
+            Assert.False(prop.IsDate);
+
+            prop.SetAsDate();
+            Assert.Equal(now.Date, prop.Value);
+            Assert.True(prop.IsDate);
+
+            prop.SetAsDateTime();
+            Assert.Equal(now.Date, prop.Value);
+            Assert.False(prop.IsDate);
+
+            prop.SetAsDate(now.AddDays(-1));
+            Assert.Equal(now.Date.AddDays(-1), prop.Value);
+            Assert.True(prop.IsDate);
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            DateTime dt = new DateTime(2017, 11, 26, 18, 25, 12);
+            var prop = new TypedDateTimeProperty()
+            {
+                Name = "MyDate",
+                Value = dt,
+                TimeZoneID = "TimeZone"
+            };
+
+            var parser = new CalendarParser();
+            StringBuilder output = new StringBuilder();
+            using (var source = new StringWriter(output))
+            {
+                var writer = new CalTextWriter(parser, source);
+                prop.Serialize(writer);
+                prop.SetAsDate();
+                prop.Serialize(writer);
+            }
+
+            Assert.Equal(new StringBuilder()
+                .AppendLine("MYDATE;TZID=TimeZone:20171126T182512")
+                .AppendLine("MYDATE;TZID=TimeZone;VALUE=DATE:20171126")
+                .ToString(), output.ToString());
+
+            string input = new StringBuilder()
+                .AppendLine("MYDATE;TZID=TimeZone:20171126T182512")
+                .AppendLine("MYDATE;VALUE=DATE:20171126")
+                .AppendLine("Other:Test")
+                .ToString();
+            using (var source = new StringReader(input))
+            {
+                var reader = new CalTextReader(parser, source, false);
+
+                var line = reader.ReadNextLine();
+                Assert.NotNull(line);
+                prop = reader.MakeProperty<TypedDateTimeProperty>(line);
+                Assert.Equal("MYDATE", prop.Name);
+                Assert.Equal(dt, prop.Value);
+                Assert.False(prop.IsDate);
+                Assert.Equal("TimeZone", prop.TimeZoneID);
+
+                line = reader.ReadNextLine();
+                Assert.NotNull(line);
+                prop = reader.MakeProperty<TypedDateTimeProperty>(line);
+                Assert.Equal("MYDATE", prop.Name);
+                Assert.Equal(dt.Date, prop.Value);
+                Assert.True(prop.IsDate);
+                Assert.Null(prop.TimeZoneID);
+
+                line = reader.ReadNextLine();
+                Assert.NotNull(line);
+                prop = reader.MakeProperty<TypedDateTimeProperty>(line);
+                Assert.Equal(DateTime.MinValue, prop.Value);
+
+                line = reader.ReadNextLine();
+                Assert.Null(line);
+            }
+
+        }
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Structure/Properties/TypedDateTimeProperty.cs
@@ -0,0 +1,135 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar.Structure
+{
+    /// <summary>
+    /// A datetime with a type (date or datetime)
+    /// </summary>
+    public class TypedDateTimeProperty : DateTimeProperty
+    {
+
+        #region Serialization
+
+        /// <summary>
+        /// Serialize value
+        /// </summary>
+        protected override string SerializeValue(ICalWriter writer, ContentLine line)
+        {
+            if (IsDate)
+                return writer.Parser.EncodeDate(Value);
+            return base.SerializeValue(writer, line);
+        }
+
+        /// <summary>
+        /// Deserialize a parameter
+        /// </summary>
+        protected override ICalPropertyParameter DeserializeParameter(ICalReader reader, ContentLine line, string name, string value)
+        {
+            if (name.IsEqual(Constants.VALUE))
+                return reader.MakePropertyParameter<EnumParameter<ValueTypes>>(name, value);
+            return base.DeserializeParameter(reader, line, name, value);
+        }
+
+        /// <summary>
+        /// Deserialize the value
+        /// </summary>
+        protected override bool DeserializeValue(ICalReader reader, ContentLine line)
+        {
+            var dt = reader.Parser.ParseDateTime(line.Value, false);
+            if (dt.HasValue)
+            {
+                SetAsDateTime(dt);
+                return true;
+            }
+            dt = reader.Parser.ParseDate(line.Value);
+            if (!dt.HasValue) return false;
+            SetAsDate(dt);
+            return true;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Define the property as a date
+        /// </summary>
+        public void SetAsDate(DateTime? value = null)
+        {
+            if (value.HasValue)
+            {
+                base.Value = value.Value.Date;
+            }
+            else
+            {
+                base.Value = base.Value.Date;
+            }
+            SetParameter(new EnumParameter<ValueTypes>() { Name = Constants.VALUE, Value = ValueTypes.Date });
+        }
+
+        /// <summary>
+        /// Define the property as a datetime
+        /// </summary>
+        public void SetAsDateTime(DateTime? value = null)
+        {
+            if (value.HasValue)
+            {
+                base.Value = value.Value;
+            }
+            RemoveParameter(Constants.VALUE);
+        }
+
+        /// <summary>
+        /// Cast to date
+        /// </summary>
+        public static implicit operator DateTime? (TypedDateTimeProperty prop) { return prop?.Value; }
+
+        /// <summary>
+        /// Cast from date
+        /// </summary>
+        public static implicit operator TypedDateTimeProperty(DateTime? value) { return value != null ? new TypedDateTimeProperty { Value = value.Value } : null; }
+
+        /// <summary>
+        /// Cast to date
+        /// </summary>
+        public static implicit operator DateTime(TypedDateTimeProperty prop) { return prop?.Value ?? DateTime.MinValue; }
+
+        /// <summary>
+        /// Cast from date
+        /// </summary>
+        public static implicit operator TypedDateTimeProperty(DateTime value) { return new TypedDateTimeProperty { Value = value }; }
+
+        /// <summary>
+        /// Indicates if the value is a date. If false it's a datetime.
+        /// </summary>
+        public bool IsDate => FindParameter<EnumParameter<ValueTypes>>(Constants.VALUE)?.Value == ValueTypes.Date;
+
+        /// <summary>
+        /// Date value
+        /// </summary>
+        public override DateTime Value
+        {
+            get { return IsDate ? base.Value.Date : base.Value; }
+            set
+            {
+                base.Value = value;
+                if (Value.Date == Value)
+                    SetAsDate();
+                else
+                    SetAsDateTime();
+            }
+        }
+
+        /// <summary>
+        /// The time zone ID
+        /// </summary>
+        public TextParameter TimeZoneID
+        {
+            get { return FindParameter<TextParameter>(Constants.TZID); }
+            set { SetParameter(value, Constants.TZID); }
+        }
+
+    }
+}
