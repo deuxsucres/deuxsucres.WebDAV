0f239a3c1dfa42cc767d180f6290acf082d39665
Yanos <ygrenier@ygrenier.com>
2017-11-26 10:41:12 +0100

    Implements RDATE property.

---
+++ sources/deuxsucres.iCalendar.Tests/Objects/Properties/RecurDatePropertyTest.cs
@@ -0,0 +1,229 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Objects.Properties
+{
+    public class RecurDatePropertyTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var prop = new RecurDateProperty();
+            Assert.Equal("RDATE", prop.Name);
+            Assert.Equal(ValueTypes.Date_Time, prop.ValueType);
+            Assert.Null(prop.DatesValue);
+            Assert.Null(prop.PeriodsValue);
+            Assert.Null(prop.TimeZoneID);
+        }
+
+        [Fact]
+        public void Properties()
+        {
+            var prop = new RecurDateProperty();
+            Assert.Equal("RDATE", prop.Name);
+            Assert.Equal(ValueTypes.Date_Time, prop.ValueType);
+            Assert.Null(prop.DatesValue);
+            Assert.Null(prop.PeriodsValue);
+            Assert.Null(prop.TimeZoneID);
+
+            prop.PeriodsValue = new List<Period>();
+            prop.TimeZoneID = "test";
+            Assert.Equal(ValueTypes.Period, prop.ValueType);
+            Assert.Null(prop.DatesValue);
+            Assert.NotNull(prop.PeriodsValue);
+            Assert.Equal("test", prop.TimeZoneID);
+
+            prop.DatesValue = new List<DateTime>();
+            prop.TimeZoneID = null;
+            Assert.Equal(ValueTypes.Date_Time, prop.ValueType);
+            Assert.NotNull(prop.DatesValue);
+            Assert.Null(prop.PeriodsValue);
+            Assert.Null(prop.TimeZoneID);
+        }
+
+        [Fact]
+        public void Cast()
+        {
+            RecurDateProperty prop = new RecurDateProperty();
+            Assert.Equal(ValueTypes.Date_Time, prop.ValueType);
+            Assert.Null((DateTime[])prop);
+            Assert.Null((Period[])prop);
+
+            prop = new Period[0];
+            Assert.Equal(ValueTypes.Period, prop.ValueType);
+            Assert.Null((DateTime[])prop);
+            Assert.NotNull((Period[])prop);
+
+            prop = new DateTime[0];
+            Assert.Equal(ValueTypes.Date_Time, prop.ValueType);
+            Assert.NotNull((DateTime[])prop);
+            Assert.Null((Period[])prop);
+        }
+
+        [Fact]
+        public void SetValues()
+        {
+            RecurDateProperty prop = new RecurDateProperty();
+            Assert.Equal(ValueTypes.Date_Time, prop.ValueType);
+            Assert.Null(prop.DatesValue);
+            Assert.Null(prop.PeriodsValue);
+
+            var dates = new DateTime[] { DateTime.MinValue, DateTime.Now, DateTime.MaxValue };
+            prop.SetAsDate();
+            Assert.Equal(ValueTypes.Date, prop.ValueType);
+            Assert.Null(prop.DatesValue);
+            Assert.Null(prop.PeriodsValue);
+
+            prop.SetAsDateTime(dates);
+            Assert.Equal(ValueTypes.Date_Time, prop.ValueType);
+            Assert.NotNull(prop.DatesValue);
+            Assert.Equal(dates, prop.DatesValue);
+            Assert.Null(prop.PeriodsValue);
+
+            prop.SetAsPeriod();
+            Assert.Equal(ValueTypes.Period, prop.ValueType);
+            Assert.Null(prop.DatesValue);
+            Assert.Null(prop.PeriodsValue);
+
+            prop.SetAsDate(dates);
+            Assert.Equal(ValueTypes.Date, prop.ValueType);
+            Assert.NotNull(prop.DatesValue);
+            Assert.Equal(dates, prop.DatesValue);
+            Assert.Null(prop.PeriodsValue);
+
+            prop.SetAsDateTime();
+            Assert.Equal(ValueTypes.Date_Time, prop.ValueType);
+            Assert.NotNull(prop.DatesValue);
+            Assert.Equal(dates, prop.DatesValue);
+            Assert.Null(prop.PeriodsValue);
+
+            var periods = new Period[] { new Period(DateTime.Now, DateTime.Now.AddDays(2)) };
+            prop.SetAsPeriod(periods);
+            Assert.Equal(ValueTypes.Period, prop.ValueType);
+            Assert.Null(prop.DatesValue);
+            Assert.NotNull(prop.PeriodsValue);
+            Assert.Equal(periods, prop.PeriodsValue);
+            prop.SetAsPeriod();
+            Assert.Equal(ValueTypes.Period, prop.ValueType);
+            Assert.Null(prop.DatesValue);
+            Assert.NotNull(prop.PeriodsValue);
+            Assert.Equal(periods, prop.PeriodsValue);
+
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            List<RecurDateProperty> props = new List<RecurDateProperty>();
+            var parser = new CalendarParser();
+            string input = new StringBuilder()
+                .AppendLine("RDATE:19970714T123000Z")
+                .AppendLine("RDATE;TZID=America/New_York:19970714T083000")
+                .AppendLine("RDATE;VALUE=PERIOD:19960403T020000Z/19960403T040000Z,")
+                .AppendLine(" 19960404T010000Z/PT3H")
+                .AppendLine("RDATE;VALUE=DATE:19970101,19970120,19970217,19970421,")
+                .AppendLine(" 19970526,19970704,19970901,19971014,19971128,19971129,19971225")
+                .ToString();
+            using (var source = new StringReader(input))
+            {
+                var reader = new CalTextReader(parser, source, false);
+
+                var line = reader.ReadNextLine();
+                Assert.NotNull(line);
+                var prop = reader.MakeProperty<RecurDateProperty>(line);
+                props.Add(prop);
+                Assert.Equal(ValueTypes.Date_Time, prop.ValueType);
+                Assert.Null(prop.TimeZoneID);
+                Assert.Equal(new DateTime[] { new DateTime(1997, 7, 14, 12, 30, 0) }, prop.DatesValue);
+                Assert.Null(prop.PeriodsValue);
+
+                line = reader.ReadNextLine();
+                Assert.NotNull(line);
+                prop = reader.MakeProperty<RecurDateProperty>(line);
+                props.Add(prop);
+                Assert.Equal(ValueTypes.Date_Time, prop.ValueType);
+                Assert.Equal("America/New_York", prop.TimeZoneID);
+                Assert.Equal(new DateTime[] { new DateTime(1997, 7, 14, 8, 30, 0) }, prop.DatesValue);
+                Assert.Null(prop.PeriodsValue);
+
+                line = reader.ReadNextLine();
+                Assert.NotNull(line);
+                prop = reader.MakeProperty<RecurDateProperty>(line);
+                props.Add(prop);
+                Assert.Equal(ValueTypes.Period, prop.ValueType);
+                Assert.Null(prop.TimeZoneID);
+                Assert.Null(prop.DatesValue);
+                Assert.Equal(new Period[] {
+                    new Period(new DateTime(1996, 4, 3, 2, 0, 0, DateTimeKind.Utc), new DateTime(1996, 4, 3, 4, 0, 0, DateTimeKind.Utc)),
+                    new Period(new DateTime(1996, 4, 4, 1, 0, 0, DateTimeKind.Utc), TimeSpan.FromHours(3))
+                }, prop.PeriodsValue);
+
+                line = reader.ReadNextLine();
+                Assert.NotNull(line);
+                prop = reader.MakeProperty<RecurDateProperty>(line);
+                props.Add(prop);
+                Assert.Equal(ValueTypes.Date, prop.ValueType);
+                Assert.Null(prop.TimeZoneID);
+                Assert.Equal(new DateTime[] {
+                    new DateTime(1997, 1, 1, 0,0,0,DateTimeKind.Local), new DateTime(1997, 1, 20, 0,0,0,DateTimeKind.Local),
+                    new DateTime(1997, 2, 17, 0,0,0,DateTimeKind.Local), new DateTime(1997, 4, 21, 0,0,0,DateTimeKind.Local),
+                    new DateTime(1997, 5, 26, 0,0,0,DateTimeKind.Local), new DateTime(1997, 7, 4, 0,0,0,DateTimeKind.Local),
+                    new DateTime(1997, 9, 1, 0,0,0,DateTimeKind.Local), new DateTime(1997, 10, 14, 0,0,0,DateTimeKind.Local),
+                    new DateTime(1997, 11, 28, 0,0,0,DateTimeKind.Local), new DateTime(1997, 11, 29, 0,0,0,DateTimeKind.Local),
+                    new DateTime(1997, 12, 25, 0,0,0,DateTimeKind.Local)
+                }, prop.DatesValue);
+                Assert.Null(prop.PeriodsValue);
+
+                line = reader.ReadNextLine();
+                Assert.Null(line);
+
+                line = new ContentLine { Name = "RDATE", Value = null };
+                line.SetParam("VALUE", "PERIOD");
+                prop = reader.MakeProperty<RecurDateProperty>(line);
+                Assert.Equal(ValueTypes.Period, prop.ValueType);
+                Assert.Null(prop.TimeZoneID);
+                Assert.Null(prop.DatesValue);
+                Assert.Equal(new Period[0], prop.PeriodsValue);
+
+                line = new ContentLine { Name = "RDATE", Value = null };
+                line.SetParam("VALUE", "DATE");
+                prop = reader.MakeProperty<RecurDateProperty>(line);
+                Assert.Equal(ValueTypes.Date, prop.ValueType);
+                Assert.Null(prop.TimeZoneID);
+                Assert.Equal(new DateTime[0], prop.DatesValue);
+                Assert.Null(prop.PeriodsValue);
+
+                line = new ContentLine { Name = "RDATE", Value = null };
+                prop = reader.MakeProperty<RecurDateProperty>(line);
+                Assert.Equal(ValueTypes.Date_Time, prop.ValueType);
+                Assert.Null(prop.TimeZoneID);
+                Assert.Equal(new DateTime[0], prop.DatesValue);
+                Assert.Null(prop.PeriodsValue);
+            }
+
+            StringBuilder output = new StringBuilder();
+            using (var source = new StringWriter(output))
+            {
+                var writer = new CalTextWriter(parser, source);
+                foreach (var prop in props)
+                    prop.Serialize(writer);
+            }
+
+            Assert.Equal(new StringBuilder()
+                .AppendLine("RDATE:19970714T123000Z")
+                .AppendLine("RDATE;TZID=America/New_York:19970714T083000")
+                .AppendLine("RDATE;VALUE=PERIOD:19960403T020000Z/19960403T040000Z,19960404T010000Z/PT3H")
+                .AppendLine("RDATE;VALUE=DATE:19970101,19970120,19970217,19970421,19970526,19970704,1997")
+                .AppendLine(" 0901,19971014,19971128,19971129,19971225")
+                .ToString(), output.ToString());
+        }
+
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Objects/Properties/RecurDateProperty.cs
@@ -0,0 +1,186 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace deuxsucres.iCalendar
+{
+    /// <summary>
+    /// RDATE
+    /// </summary>
+    public class RecurDateProperty : CalProperty
+    {
+
+        /// <summary>
+        /// New property
+        /// </summary>
+        public RecurDateProperty()
+        {
+            Name = Constants.RDATE;
+        }
+
+        /// <summary>
+        /// Reset
+        /// </summary>
+        public override void Reset()
+        {
+            base.Reset();
+            _periodsValue = null;
+            _datesValue = null;
+        }
+
+        #region Serialization
+
+        /// <summary>
+        /// Serialize the value
+        /// </summary>
+        protected override string SerializeValue(ICalWriter writer, ContentLine line)
+        {
+            switch (ValueType)
+            {
+                case ValueTypes.Period:
+                    return writer.Parser.EncodeList(PeriodsValue, p => writer.Parser.EncodePeriod(p));
+                case ValueTypes.Date:
+                    return writer.Parser.EncodeList(DatesValue, d => writer.Parser.EncodeDate(d));
+                case ValueTypes.Date_Time:
+                default:
+                    return writer.Parser.EncodeList(DatesValue, d => writer.Parser.EncodeDateTime(d));
+            }
+        }
+
+        /// <summary>
+        /// Deserialize the value
+        /// </summary>
+        protected override bool DeserializeValue(ICalReader reader, ContentLine line)
+        {
+            var vt = reader.Parser.ParseEnum<ValueTypes>(line.GetParam(Constants.VALUE)) ?? ValueTypes.Date_Time;
+            //if (vt != ValueTypes.Date_Time && vt != ValueTypes.Date && vt != ValueTypes.Period)
+            //    vt = ValueTypes.Date_Time;
+            if (vt == ValueTypes.Period)
+            {
+                PeriodsValue = reader.Parser
+                    .ParseList(line.Value, n => reader.Parser.ParsePeriod(n))
+                    .Where(d => d != null)
+                    .Select(d => d)
+                    .ToList();
+            }
+            else
+            {
+                var dts = vt == ValueTypes.Date
+                    ? reader.Parser.ParseList(line.Value, n => reader.Parser.ParseDate(n))
+                    : reader.Parser.ParseList(line.Value, n => reader.Parser.ParseDateTime(n));
+                DatesValue = dts
+                    .Where(d => d.HasValue)
+                    .Select(d => d.Value)
+                    .ToList();
+            }
+            return true;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Define the property as a date
+        /// </summary>
+        public void SetAsDate(IEnumerable<DateTime> dates = null)
+        {
+            SetParameter(new EnumParameter<ValueTypes>() { Name = Constants.VALUE, Value = ValueTypes.Date });
+            _periodsValue = null;
+            _datesValue = dates?.ToList() ?? _datesValue;
+        }
+
+        /// <summary>
+        /// Define the property as a datetime
+        /// </summary>
+        public void SetAsDateTime(IEnumerable<DateTime> dates = null)
+        {
+            RemoveParameter(Constants.VALUE);
+            _periodsValue = null;
+            _datesValue = dates?.ToList() ?? _datesValue;
+        }
+
+        /// <summary>
+        /// Define the property as a period
+        /// </summary>
+        public void SetAsPeriod(IEnumerable<Period> periods = null)
+        {
+            SetParameter(new EnumParameter<ValueTypes>() { Name = Constants.VALUE, Value = ValueTypes.Period });
+            _datesValue = null;
+            _periodsValue = periods?.ToList() ?? _periodsValue;
+        }
+
+        /// <summary>
+        /// Cast to DateTime array
+        /// </summary>
+        public static implicit operator DateTime[] (RecurDateProperty prop) => prop?.DatesValue?.ToArray();
+
+        /// <summary>
+        /// Cast from DateTime array
+        /// </summary>
+        public static implicit operator RecurDateProperty(DateTime[] dates) => new RecurDateProperty { DatesValue = dates?.ToList() };
+
+        /// <summary>
+        /// Cast to Period array
+        /// </summary>
+        public static implicit operator Period[] (RecurDateProperty prop) => prop?.PeriodsValue?.ToArray();
+
+        /// <summary>
+        /// Cast from Period array
+        /// </summary>
+        public static implicit operator RecurDateProperty (Period[] periods) => new RecurDateProperty { PeriodsValue = periods?.ToList() };
+
+        /// <summary>
+        /// Indicates the value types
+        /// </summary>
+        public ValueTypes ValueType => FindParameter<EnumParameter<ValueTypes>>(Constants.VALUE)?.Value ?? ValueTypes.Date_Time;
+
+        /// <summary>
+        /// Date values
+        /// </summary>
+        public List<DateTime> DatesValue
+        {
+            get { return _datesValue; }
+            set
+            {
+                _datesValue = value;
+                if (_datesValue != null)
+                {
+                    _periodsValue = null;
+                    RemoveParameter(Constants.VALUE);
+                }
+            }
+        }
+        private List<DateTime> _datesValue;
+
+        /// <summary>
+        /// Period values
+        /// </summary>
+        public List<Period> PeriodsValue
+        {
+            get { return _periodsValue; }
+            set
+            {
+                _periodsValue = value;
+                if (_periodsValue != null)
+                {
+                    _datesValue = null;
+                    SetParameter(new EnumParameter<ValueTypes>() { Name = Constants.VALUE, Value = ValueTypes.Period });
+                }
+            }
+        }
+        private List<Period> _periodsValue;
+
+        /// <summary>
+        /// The time zone ID
+        /// </summary>
+        public TextParameter TimeZoneID
+        {
+            get { return FindParameter<TextParameter>(Constants.TZID); }
+            set { SetParameter(value, Constants.TZID); }
+        }
+
+    }
+}
