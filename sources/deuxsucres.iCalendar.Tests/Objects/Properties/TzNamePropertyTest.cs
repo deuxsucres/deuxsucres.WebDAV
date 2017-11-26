01d1c594dab7aad0aacecad52e175c673052e443
Yanos <ygrenier@ygrenier.com>
2017-11-26 17:51:43 +0100

    Implements TZNAME property.

---
+++ sources/deuxsucres.iCalendar.Tests/Objects/Properties/TzNamePropertyTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Objects.Properties
+{
+    public class TzNamePropertyTest
+    {
+        [Fact]
+        public void Test()
+        {
+            var prop = new TzNameProperty();
+            Assert.Equal("TZNAME", prop.Name);
+            Assert.Null(prop.Language);
+
+            prop = new TzNameProperty
+            {
+                Value = "The name",
+                Language = "en"
+            };
+            Assert.Equal("TZNAME", prop.Name);
+            Assert.NotNull(prop.Language);
+            Assert.Equal("The name", prop.Value);
+            Assert.Equal("en", prop.Language);
+            Assert.Equal(1, prop.ParameterCount);
+            Assert.Equal("The name", prop);
+
+            prop = "Another name";
+            Assert.Equal("TZNAME", prop.Name);
+            Assert.Null(prop.Language);
+            Assert.Equal("Another name", prop.Value);
+            Assert.Equal(0, prop.ParameterCount);
+            Assert.Equal("Another name", prop);
+
+            prop = (string)null;
+            Assert.Null(prop);
+            Assert.Null((string)prop);
+        }
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Objects/Properties/TzNameProperty.cs
@@ -0,0 +1,42 @@
+﻿using deuxsucres.iCalendar.Structure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar
+{
+    /// <summary>
+    /// TZNAME
+    /// </summary>
+    public class TzNameProperty : TextProperty
+    {
+
+        /// <summary>
+        /// Create a new property
+        /// </summary>
+        public TzNameProperty()
+        {
+            Name = Constants.TZNAME;
+        }
+
+        /// <summary>
+        /// Cast vers un string
+        /// </summary>
+        public static implicit operator string(TzNameProperty prop) { return prop?.Value; }
+
+        /// <summary>
+        /// Cast depuis un string
+        /// </summary>
+        public static implicit operator TzNameProperty(string value) { return value != null ? new TzNameProperty { Value = value } : null; }
+
+        /// <summary>
+        /// Language
+        /// </summary>
+        public TextParameter Language
+        {
+            get { return FindParameter<TextParameter>(Constants.LANGUAGE); }
+            set { SetParameter(value, Constants.LANGUAGE); }
+        }
+
+    }
+}
