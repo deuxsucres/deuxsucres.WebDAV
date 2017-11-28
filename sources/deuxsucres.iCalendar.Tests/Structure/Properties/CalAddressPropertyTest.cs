70989d126945be3c1060bd731a0a1a3b023a36b0
Yanos <ygrenier@ygrenier.com>
2017-11-28 19:49:50 +0100

    Implements the CAL-ADDRESS property.

---
+++ sources/deuxsucres.iCalendar.Tests/Structure/Properties/CalAddressPropertyTest.cs
@@ -0,0 +1,97 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Structure.Properties
+{
+    public class CalAddressPropertyTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var prop = new CalAddressProperty();
+            Assert.Null(prop.Value);
+            Assert.Null(prop.Role);
+        }
+
+        [Fact]
+        public void Cast()
+        {
+            var prop = new CalAddressProperty();
+            Assert.Null(prop.Value);
+            Assert.Null((Uri)prop);
+            Assert.Null((string)prop);
+
+            prop = "mailto:yan@deuxsucres.com";
+            Assert.Equal(new Uri("mailto:yan@deuxsucres.com"), prop.Value);
+            Assert.Equal(new Uri("mailto:yan@deuxsucres.com"), (Uri)prop);
+            Assert.Equal("mailto:yan@deuxsucres.com", (string)prop);
+
+            prop = new Uri("mailto:yan.grenier@deuxsucres.com");
+            Assert.Equal(new Uri("mailto:yan.grenier@deuxsucres.com"), prop.Value);
+            Assert.Equal(new Uri("mailto:yan.grenier@deuxsucres.com"), (Uri)prop);
+            Assert.Equal("mailto:yan.grenier@deuxsucres.com", (string)prop);
+        }
+
+        [Fact]
+        public void TestToString()
+        {
+            var prop = new CalAddressProperty();
+            Assert.Null(prop.ToString());
+
+            prop = "mailto:yan@deuxsucres.com";
+            Assert.Equal("mailto:yan@deuxsucres.com", prop.ToString());
+
+            prop = new Uri("mailto:yan.grenier@deuxsucres.com");
+            Assert.Equal("mailto:yan.grenier@deuxsucres.com", prop.ToString());
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            var parser = new CalendarParser();
+            StringBuilder output = new StringBuilder();
+            using (var source = new StringWriter(output))
+            {
+                var writer = new CalTextWriter(parser, source);
+
+                var prop = new CalAddressProperty
+                {
+                    Name = "Organizer",
+                    Value = new Uri("mailto:yan@deuxsucres.com"),
+                    Role = Roles.Opt_Participant
+                };
+                prop.Serialize(writer);
+            }
+
+            Assert.Equal(new StringBuilder()
+                .AppendLine("ORGANIZER;ROLE=OPT-PARTICIPANT:mailto:yan@deuxsucres.com")
+                .ToString(), output.ToString());
+
+            string input = new StringBuilder()
+                .AppendLine("ORGANIZER;ROLE=Chair:mailto:contact@deuxsucres.com")
+                .ToString();
+            using (var source = new StringReader(input))
+            {
+                var reader = new CalTextReader(parser, source, false);
+                
+                var prop = new CalAddressProperty();
+                prop.Deserialize(reader, reader.ReadNextLine());
+
+                Assert.Equal("ORGANIZER", prop.Name);
+                Assert.Equal(new Uri("mailto:contact@deuxsucres.com"), prop.Value);
+                Assert.Equal(Roles.Chair, (Roles)prop.Role);
+            }
+
+        }
+
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Structure/Properties/CalAddressProperty.cs
@@ -0,0 +1,89 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar.Structure
+{
+
+    /// <summary>
+    /// CAL-ADDRESS value property
+    /// </summary>
+    public class CalAddressProperty : CalProperty
+    {
+
+        /// <summary>
+        /// Reset
+        /// </summary>
+        public override void Reset()
+        {
+            base.Reset();
+            Value = null;
+        }
+
+        #region Serialization
+
+        /// <summary>
+        /// Serialize a value
+        /// </summary>
+        protected override string SerializeValue(ICalWriter writer, ContentLine line)
+        {
+            return writer.Parser.EncodeCalAddress(Value, false);
+        }
+
+        /// <summary>
+        /// Deserialize the value
+        /// </summary>
+        protected override bool DeserializeValue(ICalReader reader, ContentLine line)
+        {
+            Value = reader.Parser.ParseUri(line.Value, false);
+            return Value != null;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// To string
+        /// </summary>
+        public override string ToString()
+        {
+            return Value?.ToString();
+        }
+
+        /// <summary>
+        /// Cast vers un string
+        /// </summary>
+        public static implicit operator Uri(CalAddressProperty prop) { return prop?.Value; }
+
+        /// <summary>
+        /// Cast depuis un string
+        /// </summary>
+        public static implicit operator CalAddressProperty(Uri value) { return value != null ? new CalAddressProperty { Value = value } : null; }
+
+        /// <summary>
+        /// Cast to string
+        /// </summary>
+        public static implicit operator string(CalAddressProperty param) => param?.Value?.ToString();
+
+        /// <summary>
+        /// Cast from string
+        /// </summary>
+        public static implicit operator CalAddressProperty(string str) => str != null ? new CalAddressProperty { Value = new Uri(str) } : null;
+
+        /// <summary>
+        /// Uri value
+        /// </summary>
+        public Uri Value { get; set; }
+
+        /// <summary>
+        /// Role
+        /// </summary>
+        public EnumParameter<Roles> Role
+        {
+            get { return FindParameter<EnumParameter<Roles>>(Constants.ROLE); }
+            set { SetParameter(value, Constants.ROLE); }
+        }
+    }
+
+}
