45d567a4aa4d4291b52d7db514a451711a5486cf
Yanos <ygrenier@ygrenier.com>
2017-11-28 21:07:38 +0100

    Implements ORGANIZER property.

---
+++ sources/deuxsucres.iCalendar.Tests/Objects/Properties/OrganizerPropertyTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Objects.Properties
+{
+    public class OrganizerPropertyTest
+    {
+
+        [Fact]
+        public void Cast()
+        {
+            var prop = new OrganizerProperty();
+            Assert.Null(prop.Value);
+            Assert.Null((Uri)prop);
+            Assert.Null((string)prop);
+
+            prop = "mailto:yan@deuxsucres.com";
+            Assert.Equal(new Uri("mailto:yan@deuxsucres.com"), prop.Value);
+            Assert.Equal(new Uri("mailto:yan@deuxsucres.com"), (Uri)prop);
+            Assert.Equal("mailto:yan@deuxsucres.com", (string)prop);
+
+            prop = new Uri("mailto:yan.grenier@deuxsucres.com");
+            Assert.Equal(new Uri("mailto:yan.grenier@deuxsucres.com"), prop.Value);
+            Assert.Equal(new Uri("mailto:yan.grenier@deuxsucres.com"), (Uri)prop);
+            Assert.Equal("mailto:yan.grenier@deuxsucres.com", (string)prop);
+        }
+
+        [Fact]
+        public void Parameters()
+        {
+            var prop = new OrganizerProperty();
+            Assert.Equal("ORGANIZER", prop.Name);
+            Assert.Null(prop.Value);
+            Assert.Null(prop.Language);
+            Assert.Null(prop.CN);
+            Assert.Null(prop.Dir);
+            Assert.Null(prop.Role);
+            Assert.Null(prop.SentBy);
+
+            prop.Value = new Uri("mailto:yan@deuxsucres");
+            prop.Language="FR";
+            prop.CN = "CN";
+            prop.Dir = "http://www.deuxsucres.com";
+            prop.Role = Roles.Chair;
+            prop.SentBy = "mailto:yan.grenier@deuxsucres.com";
+
+            Assert.Equal(new Uri("mailto:yan@deuxsucres"), prop.Value);
+            Assert.Equal("FR", prop.Language);
+            Assert.Equal("CN", prop.CN);
+            Assert.Equal("http://www.deuxsucres.com/", prop.Dir);
+            Assert.Equal(Roles.Chair, (Roles)prop.Role);
+            Assert.Equal("mailto:yan.grenier@deuxsucres.com", prop.SentBy);
+        }
+
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Objects/Properties/OrganizerProperty.cs
@@ -0,0 +1,78 @@
+﻿using deuxsucres.iCalendar.Structure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar
+{
+    /// <summary>
+    /// ORGANIZER
+    /// </summary>
+    public class OrganizerProperty : CalAddressProperty
+    {
+        /// <summary>
+        /// New property
+        /// </summary>
+        public OrganizerProperty()
+        {
+            Name = Constants.ORGANIZER;
+        }
+
+        /// <summary>
+        /// Cast to uri
+        /// </summary>
+        public static implicit operator Uri(OrganizerProperty prop) { return prop?.Value; }
+
+        /// <summary>
+        /// Cast from uri
+        /// </summary>
+        public static implicit operator OrganizerProperty(Uri value) { return value != null ? new OrganizerProperty { Value = value } : null; }
+
+        /// <summary>
+        /// Cast to string
+        /// </summary>
+        public static implicit operator string(OrganizerProperty prop) { return prop?.Value?.ToString(); }
+
+        /// <summary>
+        /// Cast from string
+        /// </summary>
+        public static implicit operator OrganizerProperty(string value) { return value != null ? new OrganizerProperty { Value = new Uri(value) } : null; }
+
+        /// <summary>
+        /// CN
+        /// </summary>
+        public TextParameter CN
+        {
+            get { return FindParameter<TextParameter>(Constants.CN); }
+            set { SetParameter(value, Constants.CN); }
+        }
+
+        /// <summary>
+        /// Dir
+        /// </summary>
+        public UriParameter Dir
+        {
+            get { return FindParameter<UriParameter>(Constants.DIR); }
+            set { SetParameter(value, Constants.DIR); }
+        }
+
+        /// <summary>
+        /// Sent by
+        /// </summary>
+        public CalAddressParameter SentBy
+        {
+            get { return FindParameter<CalAddressParameter>(Constants.SENT_BY); }
+            set { SetParameter(value, Constants.SENT_BY); }
+        }
+
+        /// <summary>
+        /// Language
+        /// </summary>
+        public TextParameter Language
+        {
+            get { return FindParameter<TextParameter>(Constants.LANGUAGE); }
+            set { SetParameter(value, Constants.LANGUAGE); }
+        }
+
+    }
+}
