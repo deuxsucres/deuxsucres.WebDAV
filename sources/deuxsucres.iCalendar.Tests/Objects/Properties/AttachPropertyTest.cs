d43dee34c2b6c2d121ffc9bac18ec7ac86a16f72
Yanos <ygrenier@ygrenier.com>
2017-12-01 18:44:06 +0100

    Implements ATTACH property.

---
+++ sources/deuxsucres.iCalendar.Tests/Objects/Properties/AttachPropertyTest.cs
@@ -0,0 +1,165 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Objects.Properties
+{
+    public class AttachPropertyTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var prop = new AttachProperty();
+            Assert.Equal(Constants.ATTACH, prop.Name);
+            Assert.False(prop.IsBinary);
+            Assert.Null(prop.BinaryValue);
+            Assert.Null(prop.UriValue);
+            Assert.Null(prop.FormatType);
+        }
+
+        [Fact]
+        public void Properties()
+        {
+            var prop = new AttachProperty();
+            Assert.False(prop.IsBinary);
+            Assert.Null(prop.BinaryValue);
+            Assert.Null(prop.UriValue);
+            Assert.Null(prop.FormatType);
+
+            prop.FormatType = "Test";
+            Assert.Equal("Test", prop.FormatType);
+            prop.FormatType = null;
+            Assert.Null(prop.FormatType);
+
+            var data = new byte[] { 1, 2, 3, 4, 5 };
+            prop.BinaryValue = data;
+            Assert.True(prop.IsBinary);
+            Assert.NotNull(prop.BinaryValue);
+            Assert.Equal(data, prop.BinaryValue);
+            Assert.Null(prop.UriValue);
+            Assert.Equal(2, prop.ParameterCount);
+
+            var uri = new Uri("http://www.deuxsucres.com");
+            prop.UriValue = uri;
+            Assert.False(prop.IsBinary);
+            Assert.Null(prop.BinaryValue);
+            Assert.NotNull(prop.UriValue);
+            Assert.Equal("http://www.deuxsucres.com/", prop.UriValue.ToString());
+            Assert.Equal(0, prop.ParameterCount);
+        }
+
+        [Fact]
+        public void Cast()
+        {
+            var prop = new AttachProperty();
+
+            var data = new byte[] { 1, 2, 3, 4, 5 };
+            prop = data;
+            Assert.True(prop.IsBinary);
+            Assert.NotNull(prop.BinaryValue);
+            Assert.Equal(data, prop.BinaryValue);
+            Assert.Null(prop.UriValue);
+            Assert.Equal(2, prop.ParameterCount);
+
+            var uri = new Uri("http://www.deuxsucres.com");
+            prop = uri;
+            Assert.False(prop.IsBinary);
+            Assert.Null(prop.BinaryValue);
+            Assert.NotNull(prop.UriValue);
+            Assert.Equal("http://www.deuxsucres.com/", prop.UriValue.ToString());
+            Assert.Equal(0, prop.ParameterCount);
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            var data = new byte[] { 1, 2, 3, 4, 5 };
+            var uri = new Uri("http://www.deuxsucres.com");
+
+            var parser = new CalendarParser();
+            var output = new StringBuilder();
+            using (var source = new StringWriter(output))
+            {
+                var writer = new CalTextWriter(parser, source);
+
+                var prop = new AttachProperty
+                {
+                    BinaryValue = data,
+                    FormatType = "application/binary"
+                };
+                prop.Serialize(writer);
+
+                prop = new AttachProperty
+                {
+                    UriValue = uri
+                };
+                prop.Serialize(writer);
+
+                prop = new AttachProperty
+                {
+                    BinaryValue = data
+                };
+                prop.RemoveParameter(Constants.VALUE);
+                prop.RemoveParameter(Constants.ENCODING);
+                prop.Serialize(writer);
+
+            }
+
+            Assert.Equal(new StringBuilder()
+                .AppendLine("ATTACH;ENCODING=Base64;VALUE=BINARY;FMTTYPE=application/binary:AQIDBAU=")
+                .AppendLine("ATTACH:http://www.deuxsucres.com/")
+                .AppendLine("ATTACH;ENCODING=Base64;VALUE=BINARY:AQIDBAU=")
+                .ToString(), output.ToString());
+
+            string input = output.ToString();
+            using (var source = new StringReader(input))
+            {
+                var reader = new CalTextReader(parser, source, false);
+
+                var prop = reader.MakeProperty<AttachProperty>(reader.ReadNextLine());
+                Assert.NotNull(prop);
+                Assert.Equal(Constants.ATTACH, prop.Name);
+                Assert.Equal("application/binary", prop.FormatType);
+                Assert.True(prop.IsBinary);
+                Assert.Null(prop.UriValue);
+                Assert.Equal(data, prop.BinaryValue);
+                Assert.Equal(3, prop.ParameterCount);
+                Assert.Equal("application/binary", prop.FindParameter<TextParameter>(Constants.FMTTYPE).Value);
+                Assert.Equal("Base64", prop.FindParameter<TextParameter>(Constants.ENCODING).Value);
+                Assert.Equal(ValueTypes.Binary, prop.FindParameter<EnumParameter<ValueTypes>>(Constants.VALUE).Value);
+
+                prop = reader.MakeProperty<AttachProperty>(reader.ReadNextLine());
+                Assert.NotNull(prop);
+                Assert.Equal(Constants.ATTACH, prop.Name);
+                Assert.Null(prop.FormatType);
+                Assert.False(prop.IsBinary);
+                Assert.Equal("http://www.deuxsucres.com/", prop.UriValue.ToString());
+                Assert.Null(prop.BinaryValue);
+                Assert.Equal(0, prop.ParameterCount);
+
+                prop = reader.MakeProperty<AttachProperty>(reader.ReadNextLine());
+                Assert.NotNull(prop);
+                Assert.Equal(Constants.ATTACH, prop.Name);
+                Assert.Null(prop.FormatType);
+                Assert.True(prop.IsBinary);
+                Assert.Null(prop.UriValue);
+                Assert.Equal(data, prop.BinaryValue);
+                Assert.Equal(2, prop.ParameterCount);
+                Assert.Equal("Base64", prop.FindParameter<TextParameter>(Constants.ENCODING).Value);
+                Assert.Equal(ValueTypes.Binary, prop.FindParameter<EnumParameter<ValueTypes>>(Constants.VALUE).Value);
+
+                prop = reader.MakeProperty<AttachProperty>(reader.ReadNextLine());
+                Assert.Null(prop);
+            }
+
+        }
+
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Objects/Properties/AttachProperty.cs
@@ -0,0 +1,135 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar
+{
+    /// <summary>
+    /// ATTACH property
+    /// </summary>
+    public class AttachProperty : CalProperty
+    {
+
+        /// <summary>
+        /// New property
+        /// </summary>
+        public AttachProperty()
+        {
+            Name = Constants.ATTACH;
+        }
+
+        /// <summary>
+        /// Check the parameters from the context
+        /// </summary>
+        void CheckParameters()
+        {
+            if (IsBinary)
+            {
+                SetParameter(new TextParameter { Name = Constants.ENCODING, Value = "Base64" });
+                SetParameter(new EnumParameter<ValueTypes> { Name = Constants.VALUE, Value = ValueTypes.Binary });
+            }
+            else
+            {
+                if (FindParameter<TextParameter>(Constants.ENCODING)?.Value.IsEqual("Base64") == true)
+                    RemoveParameter(Constants.ENCODING);
+                if (FindParameter<EnumParameter<ValueTypes>>(Constants.VALUE)?.Value == ValueTypes.Binary)
+                    RemoveParameter(Constants.VALUE);
+            }
+        }
+
+        #region Serialization
+
+        /// <summary>
+        /// Serialize the value
+        /// </summary>
+        protected override string SerializeValue(ICalWriter writer, ContentLine line)
+        {
+            CheckParameters();
+            return writer.Parser.EncodeBinary(BinaryValue) ?? writer.Parser.EncodeUri(UriValue, false);
+        }
+
+        /// <summary>
+        /// Deserialize the value
+        /// </summary>
+        protected override bool DeserializeValue(ICalReader reader, ContentLine line)
+        {
+            if (line.GetParam(Constants.ENCODING).IsEqual("Base64") && line.GetParam(Constants.VALUE).IsEqual("Binary"))
+            {
+                BinaryValue = reader.Parser.ParseBinary(line.Value);
+            }
+            else
+            {
+                UriValue = reader.Parser.ParseUri(line.Value, false);
+            }
+            return BinaryValue != null || UriValue != null;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Cast from Uri
+        /// </summary>
+        public static implicit operator AttachProperty(Uri value) { return value != null ? new AttachProperty { UriValue = value } : null; }
+
+        /// <summary>
+        /// Cast from bytes
+        /// </summary>
+        public static implicit operator AttachProperty(byte[] value) { return value != null ? new AttachProperty { BinaryValue = value } : null; }
+
+        /// <summary>
+        /// Uri value
+        /// </summary>
+        public Uri UriValue
+        {
+            get { return _uriValue; }
+            set
+            {
+                _uriValue = value;
+                if (_uriValue != null)
+                {
+                    _binaryValue = null;
+                    IsBinary = false;
+                }
+                CheckParameters();
+            }
+        }
+        private Uri _uriValue;
+
+        /// <summary>
+        /// Binary value
+        /// </summary>
+        public byte[] BinaryValue
+        {
+            get { return _binaryValue; }
+            set
+            {
+                _binaryValue = value;
+                if (_binaryValue != null)
+                {
+                    _uriValue = null;
+                    IsBinary = true;
+                }
+                CheckParameters();
+            }
+        }
+        private byte[] _binaryValue;
+
+        /// <summary>
+        /// Indicates if the value is binaray
+        /// </summary>
+        public bool IsBinary { get; private set; }
+
+        /// <summary>
+        /// Format type
+        /// </summary>
+        public TextParameter FormatType
+        {
+            get { return FindParameter<TextParameter>(Constants.FMTTYPE); }
+            set { SetParameter(value, Constants.FMTTYPE); }
+        }
+
+    }
+}
