15d735aa40352554ce7b000ffc4604cfd2206376
Yanos <ygrenier@ygrenier.com>
2017-12-02 07:34:35 +0100

    Implements REQUEST-STATUS property.

---
+++ sources/deuxsucres.iCalendar.Tests/Objects/Properties/RequestStatusPropertyTest.cs
@@ -0,0 +1,77 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Objects.Properties
+{
+    public class RequestStatusPropertyTest
+    {
+        [Fact]
+        public void Serialization()
+        {
+            var parser = new CalendarParser();
+            StringBuilder output = new StringBuilder();
+            using (var source = new StringWriter(output))
+            {
+                var writer = new CalTextWriter(parser, source);
+
+                RequestStatusProperty prop = new RequestStatusProperty
+                {
+                    StatusCode = "1.2",
+                    StatusDescription = "Status",
+                    ExtraData = "Extra data",
+                    Language = "fr"
+                };
+                prop.Serialize(writer);
+                prop = new RequestStatusProperty
+                {
+                    StatusCode = "3.4",
+                    StatusDescription = "Status"
+                };
+                prop.Serialize(writer);
+            }
+
+            Assert.Equal(new StringBuilder()
+                .AppendLine("REQUEST-STATUS;LANGUAGE=fr:1.2;Status;Extra data")
+                .AppendLine("REQUEST-STATUS:3.4;Status")
+                .ToString(), output.ToString());
+
+            string input = new StringBuilder()
+                .AppendLine("REQUEST-STATUS;LANGUAGE=fr:1.2;Status;Extra data")
+                .AppendLine("REQUEST-STATUS:3.4;Status")
+                .ToString();
+            using (var source = new StringReader(input))
+            {
+                var reader = new CalTextReader(parser, source, false);
+
+                var prop = new RequestStatusProperty();
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Equal("REQUEST-STATUS", prop.Name);
+                Assert.Equal("1.2", prop.StatusCode);
+                Assert.Equal("Status", prop.StatusDescription);
+                Assert.Equal("Extra data", prop.ExtraData);
+                Assert.Equal("fr", prop.Language);
+
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Equal("REQUEST-STATUS", prop.Name);
+                Assert.Equal("3.4", prop.StatusCode);
+                Assert.Equal("Status", prop.StatusDescription);
+                Assert.Null(prop.ExtraData);
+                Assert.Null(prop.Language);
+
+                prop.Deserialize(reader, new ContentLine { Name = "REQUEST-STATUS" });
+                Assert.Equal("REQUEST-STATUS", prop.Name);
+                Assert.Null(prop.StatusCode);
+                Assert.Null(prop.StatusDescription);
+                Assert.Null(prop.ExtraData);
+                Assert.Null(prop.Language);
+            }
+        }
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Objects/Properties/RequestStatusProperty.cs
@@ -0,0 +1,91 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar
+{
+
+    /// <summary>
+    /// REQUEST-STATUS property
+    /// </summary>
+    public class RequestStatusProperty : CalProperty
+    {
+
+        /// <summary>
+        /// Reset
+        /// </summary>
+        public override void Reset()
+        {
+            base.Reset();
+            StatusCode = null;
+            StatusDescription = null;
+            ExtraData = null;
+        }
+
+        /// <summary>
+        /// New property
+        /// </summary>
+        public RequestStatusProperty()
+        {
+            Name = Constants.REQUEST_STATUS;
+        }
+
+        #region Serialization
+
+        /// <summary>
+        /// Serialize the value
+        /// </summary>
+        protected override string SerializeValue(ICalWriter writer, ContentLine line)
+        {
+            return string.IsNullOrWhiteSpace(ExtraData)
+                ? $"{StatusCode};{StatusDescription}"
+                : $"{StatusCode};{StatusDescription};{ExtraData}";
+        }
+
+        /// <summary>
+        /// Deserialize the value
+        /// </summary>
+        protected override bool DeserializeValue(ICalReader reader, ContentLine line)
+        {
+            if (line.Value == null) return false;
+            var parts = line.Value.Split(new char[] { ';' }, 3);
+            if (parts.Length > 0)
+                StatusCode = parts[0];
+            if (parts.Length > 1)
+                StatusDescription = parts[1];
+            if (parts.Length > 2)
+                ExtraData = parts[2];
+            return true;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Status code
+        /// </summary>
+        public string StatusCode { get; set; }
+
+        /// <summary>
+        /// Status description
+        /// </summary>
+        public string StatusDescription { get; set; }
+
+        /// <summary>
+        /// Extra data
+        /// </summary>
+        public string ExtraData { get; set; }
+
+        /// <summary>
+        /// Language
+        /// </summary>
+        public TextParameter Language
+        {
+            get { return FindParameter<TextParameter>(Constants.LANGUAGE); }
+            set { SetParameter(value, Constants.LANGUAGE); }
+        }
+
+    }
+}
