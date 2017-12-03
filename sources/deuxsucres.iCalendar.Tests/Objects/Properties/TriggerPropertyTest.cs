cc8e2c499836a95c28fff381038a1ffd6cc9bbec
Yanos <ygrenier@ygrenier.com>
2017-12-03 09:06:20 +0100

    Implements TRIGGER property.

---
+++ sources/deuxsucres.iCalendar.Tests/Objects/Properties/TriggerPropertyTest.cs
@@ -0,0 +1,193 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Objects.Properties
+{
+    public class TriggerPropertyTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var prop = new TriggerProperty();
+            Assert.Equal("TRIGGER", prop.Name);
+            Assert.Null(prop.AbsoluteValue);
+            Assert.Null(prop.RelativeValue);
+        }
+
+        [Fact]
+        public void Properties()
+        {
+            var prop = new TriggerProperty();
+            Assert.Equal("TRIGGER", prop.Name);
+            Assert.Null(prop.AbsoluteValue);
+            Assert.Null(prop.RelativeValue);
+            Assert.Null(prop.FindParameter<EnumParameter<ValueTypes>>(Constants.VALUE));
+
+            DateTime dt = new DateTime(2017, 12, 3, 8, 18, 12);
+            TimeSpan ts = TimeSpan.FromDays(12.3456789);
+
+            prop.AbsoluteValue = dt;
+            Assert.Equal(dt, prop.AbsoluteValue);
+            Assert.Null(prop.RelativeValue);
+            Assert.Equal(ValueTypes.Date_Time, (ValueTypes)prop.FindParameter<EnumParameter<ValueTypes>>(Constants.VALUE));
+
+            prop.AbsoluteValue = dt.Date;
+            Assert.Equal(dt.Date, prop.AbsoluteValue);
+            Assert.Null(prop.RelativeValue);
+            Assert.Equal(ValueTypes.Date, (ValueTypes)prop.FindParameter<EnumParameter<ValueTypes>>(Constants.VALUE));
+
+            prop.RelativeValue = ts;
+            Assert.Null(prop.AbsoluteValue);
+            Assert.Equal(ts, prop.RelativeValue);
+            Assert.Equal(ValueTypes.Duration, (ValueTypes)prop.FindParameter<EnumParameter<ValueTypes>>(Constants.VALUE));
+        }
+
+        [Fact]
+        public void Cast()
+        {
+            var prop = new TriggerProperty();
+            Assert.Null(prop.AbsoluteValue);
+            Assert.Null(prop.RelativeValue);
+
+            DateTime dt = new DateTime(2017, 12, 3, 8, 18, 12);
+            TimeSpan ts = TimeSpan.FromDays(12.3456789);
+
+            prop = dt;
+            Assert.Equal(dt, prop.AbsoluteValue);
+            Assert.Null(prop.RelativeValue);
+            Assert.Equal(dt, (DateTime?)prop);
+            Assert.Null((TimeSpan?)prop);
+
+            prop = dt.Date;
+            Assert.Equal(dt.Date, prop.AbsoluteValue);
+            Assert.Null(prop.RelativeValue);
+            Assert.Equal(dt.Date, (DateTime?)prop);
+            Assert.Null((TimeSpan?)prop);
+
+            prop = ts;
+            Assert.Null(prop.AbsoluteValue);
+            Assert.Equal(ts, prop.RelativeValue);
+            Assert.Null((DateTime?)prop);
+            Assert.Equal(ts, (TimeSpan?)prop);
+
+            prop = (DateTime?)null;
+            Assert.Null(prop);
+
+            prop = (TimeSpan?)null;
+            Assert.Null(prop);
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            DateTime dt = new DateTime(2017, 12, 3, 8, 18, 12);
+            TimeSpan ts = TimeSpan.FromDays(12.3456789);
+
+            var parser = new CalendarParser();
+            StringBuilder output = new StringBuilder();
+            using (var source = new StringWriter(output))
+            {
+                var writer = new CalTextWriter(parser, source);
+
+                TriggerProperty prop = dt;
+                prop.Serialize(writer);
+
+                prop = dt.Date;
+                prop.Serialize(writer);
+
+                prop = ts;
+                prop.Serialize(writer);
+
+                prop = dt;
+                prop.RemoveParameter(Constants.VALUE);
+                prop.Serialize(writer);
+
+                prop = ts;
+                prop.RemoveParameter(Constants.VALUE);
+                prop.Serialize(writer);
+
+                prop.AbsoluteValue = null;
+                prop.RelativeValue = null;
+                prop.Serialize(writer);
+            }
+
+            Assert.Equal(new StringBuilder()
+                .AppendLine("TRIGGER;VALUE=DATE-TIME:20171203T081812")
+                .AppendLine("TRIGGER;VALUE=DATE:20171203")
+                .AppendLine("TRIGGER;VALUE=DURATION:P12DT8H17M46S")
+                .AppendLine("TRIGGER:20171203T081812")
+                .AppendLine("TRIGGER:P12DT8H17M46S")
+                .AppendLine("TRIGGER:")
+                .ToString(), output.ToString());
+
+            string input = new StringBuilder()
+                .AppendLine("TRIGGER;VALUE=DATE-TIME:20171203T081812")
+                .AppendLine("TRIGGER;VALUE=DATE:20171203")
+                .AppendLine("TRIGGER;VALUE=TEXT:20171203")
+                .AppendLine("TRIGGER;VALUE=DURATION:P12DT8H17M46S")
+                .AppendLine("TRIGGER:20171203T081812")
+                .AppendLine("TRIGGER:P12DT8H17M46S")
+                .AppendLine("TRIGGER:Test")
+                .AppendLine("TRIGGER:")
+                .ToString();
+            using (var source = new StringReader(input))
+            {
+                var reader = new CalTextReader(parser, source, false);
+
+                var prop = new TriggerProperty();
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Equal("TRIGGER", prop.Name);
+                Assert.Equal(dt, prop.AbsoluteValue);
+                Assert.Null(prop.RelativeValue);
+                Assert.Equal(ValueTypes.Date_Time, (ValueTypes)prop.FindParameter<EnumParameter<ValueTypes>>(Constants.VALUE));
+
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Equal(dt.Date, prop.AbsoluteValue);
+                Assert.Null(prop.RelativeValue);
+                Assert.Equal(ValueTypes.Date, (ValueTypes)prop.FindParameter<EnumParameter<ValueTypes>>(Constants.VALUE));
+
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Equal(dt.Date, prop.AbsoluteValue);
+                Assert.Null(prop.RelativeValue);
+                Assert.Equal(ValueTypes.Text, (ValueTypes)prop.FindParameter<EnumParameter<ValueTypes>>(Constants.VALUE));
+
+                TimeSpan tsd = new TimeSpan(12, 8, 17, 46);
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Null(prop.AbsoluteValue);
+                Assert.Equal(tsd, prop.RelativeValue);
+                Assert.Equal(ValueTypes.Duration, (ValueTypes)prop.FindParameter<EnumParameter<ValueTypes>>(Constants.VALUE));
+
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Equal(dt, prop.AbsoluteValue);
+                Assert.Null(prop.RelativeValue);
+                Assert.Equal(ValueTypes.Date_Time, (ValueTypes)prop.FindParameter<EnumParameter<ValueTypes>>(Constants.VALUE));
+
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Null(prop.AbsoluteValue);
+                Assert.Equal(tsd, prop.RelativeValue);
+                Assert.Equal(ValueTypes.Duration, (ValueTypes)prop.FindParameter<EnumParameter<ValueTypes>>(Constants.VALUE));
+
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Null(prop.AbsoluteValue);
+                Assert.Null(prop.RelativeValue);
+                Assert.Null(prop.FindParameter<EnumParameter<ValueTypes>>(Constants.VALUE));
+
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Null(prop.AbsoluteValue);
+                Assert.Null(prop.RelativeValue);
+                Assert.Null(prop.FindParameter<EnumParameter<ValueTypes>>(Constants.VALUE));
+
+            }
+
+        }
+
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Objects/Properties/TriggerProperty.cs
@@ -0,0 +1,151 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar
+{
+    /// <summary>
+    /// TRIGGER property
+    /// </summary>
+    public class TriggerProperty : CalProperty
+    {
+        /// <summary>
+        /// New property
+        /// </summary>
+        public TriggerProperty()
+        {
+            Name = Constants.TRIGGER;
+        }
+
+        /// <summary>
+        /// Reset
+        /// </summary>
+        public override void Reset()
+        {
+            base.Reset();
+            RelativeValue = null;
+            AbsoluteValue = null;
+        }
+
+        #region Serialization
+
+        /// <summary>
+        /// Serialize a value
+        /// </summary>
+        protected override string SerializeValue(ICalWriter writer, ContentLine line)
+        {
+            var vt = GetParameter<EnumParameter<ValueTypes>>(Constants.VALUE)?.Value;
+            if (vt == ValueTypes.Duration)
+            {
+                return RelativeValue == null ? null : writer.Parser.EncodeDuration(RelativeValue.Value);
+            }
+            else if (vt == ValueTypes.Date_Time)
+            {
+                return AbsoluteValue == null ? null : writer.Parser.EncodeDateTime(AbsoluteValue.Value);
+            }
+            else if (vt == ValueTypes.Date)
+            {
+                return AbsoluteValue == null ? null : writer.Parser.EncodeDate(AbsoluteValue.Value);
+            }
+            string val =
+                (RelativeValue == null ? null : writer.Parser.EncodeDuration(RelativeValue.Value))
+                ?? (AbsoluteValue == null ? null : writer.Parser.EncodeDateTime(AbsoluteValue.Value));
+            return val;
+        }
+
+        /// <summary>
+        /// Deserialize the value
+        /// </summary>
+        protected override bool DeserializeValue(ICalReader reader, ContentLine line)
+        {
+            var vt = reader.Parser.ParseEnum<ValueTypes>(line.GetParam(Constants.VALUE));
+            if (vt == ValueTypes.Duration)
+            {
+                RelativeValue = reader.Parser.ParseDuration(line.Value);
+            }
+            else if (vt == ValueTypes.Date_Time || vt == ValueTypes.Date)
+            {
+                AbsoluteValue = reader.Parser.ParseDateTime(line.Value);
+            }
+            else
+            {
+                RelativeValue = reader.Parser.ParseDuration(line.Value);
+                if (RelativeValue == null)
+                    AbsoluteValue = reader.Parser.ParseDateTime(line.Value);
+            }
+            return RelativeValue.HasValue || AbsoluteValue.HasValue;
+        }
+
+        #endregion
+
+        #region Casts
+
+        /// <summary>
+        /// Cast to datetime
+        /// </summary>
+        public static implicit operator DateTime? (TriggerProperty prop) => prop?.AbsoluteValue;
+
+        /// <summary>
+        /// Cast from datetime
+        /// </summary>
+        public static implicit operator TriggerProperty(DateTime? date) => date != null ? new TriggerProperty { AbsoluteValue = date } : null;
+
+        /// <summary>
+        /// Cast to timespan
+        /// </summary>
+        public static implicit operator TimeSpan? (TriggerProperty prop) => prop?.RelativeValue;
+
+        /// <summary>
+        /// Cast from timespan
+        /// </summary>
+        public static implicit operator TriggerProperty(TimeSpan? time) => time != null ? new TriggerProperty { RelativeValue = time } : null;
+
+        #endregion
+
+        /// <summary>
+        /// Relative value in duration
+        /// </summary>
+        public TimeSpan? RelativeValue
+        {
+            get { return _relativeValue; }
+            set
+            {
+                RemoveParameter(Constants.VALUE);
+                _relativeValue = value;
+                if (_relativeValue != null)
+                {
+                    _absoluteValue = null;
+                    SetParameter(new EnumParameter<ValueTypes> { Name = Constants.VALUE, Value = ValueTypes.Duration });
+                }
+            }
+        }
+        private TimeSpan? _relativeValue;
+
+        /// <summary>
+        /// Absolute value in date
+        /// </summary>
+        public DateTime? AbsoluteValue
+        {
+            get { return _absoluteValue; }
+            set
+            {
+                RemoveParameter(Constants.VALUE);
+                _absoluteValue = value;
+                if (_absoluteValue != null)
+                {
+                    _relativeValue = null;
+                    SetParameter(new EnumParameter<ValueTypes>
+                    {
+                        Name = Constants.VALUE,
+                        Value = _absoluteValue.Value == _absoluteValue.Value.Date ? ValueTypes.Date : ValueTypes.Date_Time
+                    });
+                }
+            }
+        }
+        private DateTime? _absoluteValue;
+
+    }
+}
