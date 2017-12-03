4096bdcb7c490fb444c81ecb9b2074185372b44d
Yanos <ygrenier@ygrenier.com>
2017-12-03 08:00:29 +0100

    Implements DURATION property.

---
+++ sources/deuxsucres.iCalendar.Tests/Objects/Properties/DurationPropertyTest.cs
@@ -0,0 +1,72 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Objects.Properties
+{
+    public class DurationPropertyTest
+    {
+        [Fact]
+        public void Cast()
+        {
+            TimeSpan ts = TimeSpan.FromDays(12.3456789);
+            var prop = new DurationProperty();
+            Assert.Equal(TimeSpan.Zero, prop.Value);
+            Assert.Equal(TimeSpan.Zero, (TimeSpan)prop);
+            Assert.Equal(TimeSpan.Zero, (TimeSpan?)prop);
+
+            prop = ts;
+            Assert.Equal(ts, prop.Value);
+            Assert.Equal(ts, (TimeSpan)prop);
+            Assert.Equal(ts, (TimeSpan?)prop);
+
+            prop = (TimeSpan?)null;
+            Assert.Null(prop);
+            Assert.Equal(TimeSpan.Zero, (TimeSpan)prop);
+            Assert.Null((TimeSpan?)prop);
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            TimeSpan ts = TimeSpan.FromDays(12.3456789);
+
+            var parser = new CalendarParser();
+            StringBuilder output = new StringBuilder();
+            using (var source = new StringWriter(output))
+            {
+                var writer = new CalTextWriter(parser, source);
+
+                var prop = new DurationProperty() { Value = ts };
+                prop.Serialize(writer);
+            }
+
+            Assert.Equal(new StringBuilder()
+                .AppendLine("DURATION:P12DT8H17M46S")
+                .ToString(), output.ToString());
+
+            string input = new StringBuilder()
+                .AppendLine("DURATION:P12DT8H17M46S")
+                .AppendLine("DURATION:Test")
+                .ToString();
+            using (var source = new StringReader(input))
+            {
+                var reader = new CalTextReader(parser, source, false);
+
+                var prop = new DurationProperty() { Value = ts };
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Equal(new TimeSpan(12, 08, 17, 46), prop.Value);
+
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Equal(TimeSpan.Zero, prop.Value);
+            }
+        }
+    }
+
+}
---
+++ sources/deuxsucres.iCalendar/Objects/Properties/DurationProperty.cs
@@ -0,0 +1,81 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar
+{
+    /// <summary>
+    /// DURATION property
+    /// </summary>
+    public class DurationProperty : CalProperty
+    {
+
+        /// <summary>
+        /// New property
+        /// </summary>
+        public DurationProperty()
+        {
+            Name = Constants.DURATION;
+        }
+
+        /// <summary>
+        /// Reset
+        /// </summary>
+        public override void Reset()
+        {
+            base.Reset();
+            Value = TimeSpan.Zero;
+        }
+
+        #region Serialization
+
+        /// <summary>
+        /// Serialize this value
+        /// </summary>
+        protected override string SerializeValue(ICalWriter writer, ContentLine line)
+        {
+            return writer.Parser.EncodeDuration(Value);
+        }
+
+        /// <summary>
+        /// Deserialize the value
+        /// </summary>
+        protected override bool DeserializeValue(ICalReader reader, ContentLine line)
+        {
+            var du = reader.Parser.ParseDuration(line.Value);
+            if (!du.HasValue) return false;
+            Value = du.Value;
+            return true;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Cast to TimeSpan
+        /// </summary>
+        public static implicit operator TimeSpan? (DurationProperty prop) { return prop?.Value; }
+
+        /// <summary>
+        /// Cast from TimeSpan
+        /// </summary>
+        public static implicit operator DurationProperty(TimeSpan? value) { return value != null ? new DurationProperty { Value = value.Value } : null; }
+
+        /// <summary>
+        /// Cast to TimeSpan
+        /// </summary>
+        public static implicit operator TimeSpan(DurationProperty prop) { return prop?.Value ?? TimeSpan.Zero; }
+
+        /// <summary>
+        /// Cast from TimeSpan
+        /// </summary>
+        public static implicit operator DurationProperty(TimeSpan value) { return new DurationProperty { Value = value }; }
+
+        /// <summary>
+        /// TimeSpan value
+        /// </summary>
+        public virtual TimeSpan Value { get; set; }
+    }
+}
