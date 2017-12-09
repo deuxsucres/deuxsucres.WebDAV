692d8bdb36a91843a4abaddc8d20c4e48154a854
Yanos <ygrenier@ygrenier.com>
2017-12-09 10:59:55 +0100

    Implements GEO property.

---
+++ sources/deuxsucres.iCalendar.Tests/Objects/Properties/GeoPositionPropertyTest.cs
@@ -0,0 +1,105 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Objects.Properties
+{
+    public class GeoPositionPropertyTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var prop = new GeoPositionProperty();
+            Assert.Equal("GEO", prop.Name);
+            Assert.Equal(0, prop.Latitude);
+            Assert.Equal(0, prop.Longitude);
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            var parser = new CalendarParser();
+            StringBuilder output = new StringBuilder();
+            using (var source = new StringWriter(output))
+            {
+                var writer = new CalTextWriter(parser, source);
+
+                GeoPositionProperty prop = new GeoPositionProperty
+                {
+                    Latitude = 12.34,
+                    Longitude = -98.76
+                };
+                prop.Serialize(writer);
+
+                prop = new GeoPositionProperty
+                {
+                    Latitude = -12.34,
+                    Longitude = 98.76
+                };
+                prop.Serialize(writer);
+
+                prop = new GeoPositionProperty();
+                prop.Serialize(writer);
+            }
+
+            Assert.Equal(new StringBuilder()
+                .AppendLine("GEO:12.34;-98.76")
+                .AppendLine("GEO:-12.34;98.76")
+                .AppendLine("GEO:0;0")
+                .ToString(), output.ToString());
+
+            string input = new StringBuilder()
+                .AppendLine("GEO:12.34;-98.76")
+                .AppendLine("GEO:-12.34;98.76")
+                .AppendLine("GEO:0;0")
+                .AppendLine("GEO:0;Test")
+                .AppendLine("GEO:Test;0")
+                .AppendLine("GEO:0")
+                .AppendLine("GEO:")
+                .ToString();
+            using (var source = new StringReader(input))
+            {
+                var reader = new CalTextReader(parser, source, false);
+
+                var prop = new GeoPositionProperty();
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Equal("GEO", prop.Name);
+                Assert.Equal(12.34, prop.Latitude);
+                Assert.Equal(-98.76, prop.Longitude);
+
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Equal(-12.34, prop.Latitude);
+                Assert.Equal(98.76, prop.Longitude);
+
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Equal(0, prop.Latitude);
+                Assert.Equal(0, prop.Longitude);
+
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Equal(0, prop.Latitude);
+                Assert.Equal(0, prop.Longitude);
+
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Equal(0, prop.Latitude);
+                Assert.Equal(0, prop.Longitude);
+
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Equal(0, prop.Latitude);
+                Assert.Equal(0, prop.Longitude);
+
+                prop.Deserialize(reader, reader.ReadNextLine());
+                Assert.Equal(0, prop.Latitude);
+                Assert.Equal(0, prop.Longitude);
+
+                Assert.Null(reader.ReadNextLine());
+            }
+
+        }
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Objects/Properties/GeoPositionProperty.cs
@@ -0,0 +1,72 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar
+{
+    /// <summary>
+    /// GEO property
+    /// </summary>
+    public class GeoPositionProperty : CalProperty
+    {
+        /// <summary>
+        /// New property
+        /// </summary>
+        public GeoPositionProperty()
+        {
+            Name = Constants.GEO;
+        }
+
+        /// <summary>
+        /// Reset
+        /// </summary>
+        public override void Reset()
+        {
+            base.Reset();
+            Latitude = 0;
+            Longitude = 0;
+        }
+
+        #region Serialization
+
+        /// <summary>
+        /// Serialize the value
+        /// </summary>
+        protected override string SerializeValue(ICalWriter writer, ContentLine line)
+        {
+            return $"{writer.Parser.EncodeFloat(Latitude)};{writer.Parser.EncodeFloat(Longitude)}";
+        }
+
+        /// <summary>
+        /// Deserialize the value
+        /// </summary>
+        protected override bool DeserializeValue(ICalReader reader, ContentLine line)
+        {
+            if (string.IsNullOrWhiteSpace(line.Value)) return false;
+            var parts = line.Value.Split(';');
+            if (parts.Length != 2) return false;
+            var vla = reader.Parser.ParseFloat(parts[0]);
+            var vlo = reader.Parser.ParseFloat(parts[1]);
+            if (!vla.HasValue || !vlo.HasValue)
+                return false;
+            Latitude = vla.Value;
+            Longitude = vlo.Value;
+            return true;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Latitude
+        /// </summary>
+        public double Latitude { get; set; }
+
+        /// <summary>
+        /// Longitude
+        /// </summary>
+        public double Longitude { get; set; }
+    }
+}
