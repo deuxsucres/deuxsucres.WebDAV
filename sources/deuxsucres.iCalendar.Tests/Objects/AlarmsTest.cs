414ce9ab55f39568cfbaa0a3b9ec37fc619ca7d9
Yanos <ygrenier@ygrenier.com>
2017-12-09 08:31:47 +0100

    Implements VALARM property.

---
+++ sources/deuxsucres.iCalendar.Tests/Objects/AlarmsTest.cs
@@ -0,0 +1,134 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.iCalendar.Tests.Objects
+{
+    public class AlarmsTest
+    {
+        [Fact]
+        public void Properties()
+        {
+            var alarm = new TodoAlarm();
+            Assert.Equal("VALARM", alarm.Name);
+            Assert.Null(alarm.Action);
+            Assert.Null(alarm.Trigger);
+            Assert.Null(alarm.Duration);
+            Assert.Null(alarm.Repeat);
+            Assert.Null(alarm.Description);
+            Assert.Null(alarm.Summary);
+            Assert.Empty(alarm.Attachments);
+            Assert.Empty(alarm.Attendees);
+            Assert.Empty(alarm.GetAttendees());
+
+            DateTime dt = DateTime.Now;
+            TimeSpan duration = TimeSpan.FromMinutes(5);
+
+            alarm.Action = AlarmActions.Audio;
+            alarm.Trigger = dt;
+            alarm.Duration = duration;
+            alarm.Repeat = 2;
+            alarm.Description = "It's an alarm";
+            alarm.Summary = "An alarm";
+            alarm.Attachments.Add(new AttachProperty { UriValue = new Uri("http://www.deuxsucres.com") });
+            alarm.Attendees.Add(new TodoAttendeeProperty { Value = new Uri("mailto:yan@deuxsucres.com") });
+
+            Assert.Equal("AUDIO", alarm.Action);
+            Assert.Equal(dt, (DateTime)alarm.Trigger);
+            Assert.Equal(duration, (TimeSpan)alarm.Duration);
+            Assert.Equal(2, (int)alarm.Repeat);
+            Assert.Equal("It's an alarm", alarm.Description);
+            Assert.Equal("An alarm", alarm.Summary);
+            Assert.Single(alarm.Attachments);
+            Assert.Single(alarm.Attendees);
+            Assert.Single(alarm.GetAttendees());
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            DateTime dt = new DateTime(2017, 12, 2, 8, 15, 12);
+            TimeSpan duration = TimeSpan.FromMinutes(5);
+
+            var parser = new CalendarParser();
+            StringBuilder output = new StringBuilder();
+            using (var source = new StringWriter(output))
+            {
+                var writer = new CalTextWriter(parser, source);
+
+                var alarm = new TodoAlarm()
+                {
+                    Action = AlarmActions.Audio,
+                    Trigger = dt,
+                    Duration = duration,
+                    Repeat = 2,
+                    Description = "It's an alarm",
+                    Summary = "An alarm"
+                };
+                alarm.Attachments.Add(new AttachProperty { UriValue = new Uri("http://www.deuxsucres.com") });
+                alarm.Attendees.Add(new TodoAttendeeProperty { Value = new Uri("mailto:yan@deuxsucres.com"), PartStat = TodoPartStatuses.Needs_Action });
+
+                alarm.Serialize(writer);
+            }
+
+            Assert.Equal(new StringBuilder()
+                .AppendLine("BEGIN:VALARM")
+                .AppendLine("ACTION:AUDIO")
+                .AppendLine("TRIGGER;VALUE=DATE-TIME:20171202T081512")
+                .AppendLine("DURATION:PT5M")
+                .AppendLine("REPEAT:2")
+                .AppendLine("DESCRIPTION:It's an alarm")
+                .AppendLine("SUMMARY:An alarm")
+                .AppendLine("ATTACH:http://www.deuxsucres.com/")
+                .AppendLine("ATTENDEE;PARTSTAT=NEEDS-ACTION:mailto:yan@deuxsucres.com")
+                .AppendLine("END:VALARM")
+                .ToString(), output.ToString());
+
+            string input = new StringBuilder()
+                .AppendLine("BEGIN:VALARM")
+                .AppendLine("ACTION:AUDIO")
+                .AppendLine("TRIGGER;VALUE=DATE-TIME:20171202T081512")
+                .AppendLine("DURATION:PT5M")
+                .AppendLine("REPEAT:2")
+                .AppendLine("DESCRIPTION:It's an alarm")
+                .AppendLine("SUMMARY:An alarm")
+                .AppendLine("OTHER:Other property")
+                .AppendLine("ATTACH:http://www.deuxsucres.com/")
+                .AppendLine("ATTENDEE;PARTSTAT=NEEDS-ACTION:mailto:yan@deuxsucres.com")
+                .AppendLine("END:VALARM")
+                .ToString();
+            using (var source = new StringReader(input))
+            {
+                var reader = new CalTextReader(parser, source, false);
+                reader.ReadNextLine();
+
+                var alarm = new TodoAlarm();
+                alarm.Deserialize(reader);
+
+                Assert.Equal(9, alarm.PropertyCount);
+
+                Assert.Equal(AlarmActions.Audio, (AlarmActions)alarm.Action);
+                Assert.Equal(dt, (DateTime)alarm.Trigger);
+                Assert.Equal(duration, (TimeSpan)alarm.Duration);
+                Assert.Equal(2, (int)alarm.Repeat);
+                Assert.Equal("It's an alarm", (string)alarm.Description);
+                Assert.Equal("An alarm", (string)alarm.Summary);
+
+                Assert.Equal(1, alarm.Attachments.Count);
+                Assert.Equal("http://www.deuxsucres.com/", alarm.Attachments[0].UriValue.ToString());
+
+                Assert.Single(alarm.Attendees);
+                Assert.Equal("mailto:yan@deuxsucres.com", alarm.Attendees[0].Value.ToString());
+                Assert.Equal(TodoPartStatuses.Needs_Action, (TodoPartStatuses)alarm.Attendees[0].PartStat);
+
+                Assert.Null(reader.ReadNextLine());
+            }
+        }
+    }
+}
---
+++ sources/deuxsucres.iCalendar/Objects/Alarm.cs
@@ -0,0 +1,138 @@
+﻿using deuxsucres.iCalendar.Parser;
+using deuxsucres.iCalendar.Serialization;
+using deuxsucres.iCalendar.Structure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.iCalendar
+{
+    /// <summary>
+    /// Alarm
+    /// </summary>
+    public abstract class Alarm<TAttendeeProp, TStatus> : CalComponent
+        where TStatus : struct
+        where TAttendeeProp : AttendeeProperty<TStatus>
+    {
+        /// <summary>
+        /// Create a new alarm
+        /// </summary>
+        public Alarm()
+        {
+            Attachments = new CalProperties<AttachProperty>(Constants.ATTACH, this);
+            Attendees = new CalProperties<TAttendeeProp>(Constants.ATTENDEE, this);
+        }
+
+        /// <summary>
+        /// Process the properties
+        /// </summary>
+        protected override bool ProcessProperty(ICalReader reader, ContentLine line)
+        {
+            switch (line.Name.ToUpper())
+            {
+                case Constants.ACTION: SetProperty(reader.MakeProperty<EnumProperty<AlarmActions>>(line), Constants.ACTION); return true;
+                case Constants.TRIGGER: SetProperty(reader.MakeProperty<TriggerProperty>(line), Constants.TRIGGER); return true;
+                case Constants.DURATION: SetProperty(reader.MakeProperty<DurationProperty>(line), Constants.DURATION); return true;
+                case Constants.REPEAT: SetProperty(reader.MakeProperty<IntegerProperty>(line), Constants.REPEAT); return true;
+                case Constants.SUMMARY: SetProperty(reader.MakeProperty<ExtendedTextProperty>(line), Constants.SUMMARY); return true;
+                case Constants.DESCRIPTION: SetProperty(reader.MakeProperty<ExtendedTextProperty>(line), Constants.DESCRIPTION); return true;
+
+                case Constants.ATTACH: AddProperty(reader.MakeProperty<AttachProperty>(line)); return true;
+                case Constants.ATTENDEE: AddProperty(reader.MakeProperty<TAttendeeProp>(line)); return true;
+                default: return false;
+            }
+        }
+
+        /// <summary>
+        /// Get the attendees
+        /// </summary>
+        public virtual IEnumerable<TAttendeeProp> GetAttendees()
+        {
+            return FindProperties<TAttendeeProp>(Constants.ATTENDEE);
+        }
+
+        /// <summary>
+        /// Name of the object
+        /// </summary>
+        public override string Name => Constants.VALARM;
+
+        /// <summary>
+        /// ACTION
+        /// </summary>
+        public EnumProperty<AlarmActions> Action
+        {
+            get { return FindProperty<EnumProperty<AlarmActions>>(Constants.ACTION); }
+            set { SetProperty(value, Constants.ACTION); }
+        }
+
+        /// <summary>
+        /// TRIGGER
+        /// </summary>
+        public TriggerProperty Trigger
+        {
+            get { return FindProperty<TriggerProperty>(Constants.TRIGGER); }
+            set { SetProperty(value, Constants.TRIGGER); }
+        }
+
+        /// <summary>
+        /// DURATION
+        /// </summary>
+        public DurationProperty Duration
+        {
+            get { return FindProperty<DurationProperty>(Constants.DURATION); }
+            set { SetProperty(value, Constants.DURATION); }
+        }
+
+        /// <summary>
+        /// REPEAT
+        /// </summary>
+        public IntegerProperty Repeat
+        {
+            get { return FindProperty<IntegerProperty>(Constants.REPEAT); }
+            set { SetProperty(value, Constants.REPEAT); }
+        }
+
+        /// <summary>
+        /// DESCRIPTION
+        /// </summary>
+        public ExtendedTextProperty Description
+        {
+            get { return FindProperty<ExtendedTextProperty>(Constants.DESCRIPTION); }
+            set { SetProperty(value, Constants.DESCRIPTION); }
+        }
+
+        /// <summary>
+        /// SUMMARY
+        /// </summary>
+        public ExtendedTextProperty Summary
+        {
+            get { return FindProperty<ExtendedTextProperty>(Constants.SUMMARY); }
+            set { SetProperty(value, Constants.SUMMARY); }
+        }
+
+        /// <summary>
+        /// List of attachments
+        /// </summary>
+        public CalProperties<AttachProperty> Attachments { get; private set; }
+
+        /// <summary>
+        /// List of attendees
+        /// </summary>
+        public CalProperties<TAttendeeProp> Attendees { get; private set; }
+    }
+
+    /// <summary>
+    /// ALARM for event
+    /// </summary>
+    public class EventAlarm : Alarm<EventAttendeeProperty, EventPartStatuses>
+    {
+    }
+
+    /// <summary>
+    /// ALARM for todo
+    /// </summary>
+    public class TodoAlarm : Alarm<TodoAttendeeProperty, TodoPartStatuses>
+    {
+    }
+
+}
