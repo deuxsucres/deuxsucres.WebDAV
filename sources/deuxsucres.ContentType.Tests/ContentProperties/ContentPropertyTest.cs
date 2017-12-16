e339bd26190cdf091b5b72c03475dba552da023d
Yanos <ygrenier@ygrenier.com>
2017-12-16 15:20:10 +0100

    Define content property with parameters management.

---
+++ sources/deuxsucres.ContentType.Tests/ContentProperties/ContentPropertyTest.cs
@@ -0,0 +1,88 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.ContentType.Tests.ContentProperties
+{
+    public class ContentPropertyTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var prop = new Mock<ContentProperty>() { CallBase = true }.Object;
+            Assert.Null(prop.Name);
+            Assert.Equal(0, prop.ParameterCount);
+        }
+
+        [Fact]
+        public void Parameters()
+        {
+            var prop = new Mock<ContentProperty>() { CallBase = true }.Object;
+
+            Assert.Equal(0, prop.ParameterCount);
+            TextContentParameter p1 = new TextContentParameter { Name = "p1", Value = "v1" };
+            prop.SetParameter(p1);
+            Assert.Equal(1, prop.ParameterCount);
+            Assert.NotNull(prop.FindParameter("P1"));
+
+            prop.SetParameter(null);
+            Assert.Equal(1, prop.ParameterCount);
+
+            TextListContentParameter p2 = new TextListContentParameter { Value = new List<string> { "v2", "v3" } };
+            prop.SetParameter(p2, "p2");
+            Assert.Equal(2, prop.ParameterCount);
+            Assert.NotNull(prop.FindParameter("P2"));
+
+            TextContentParameter p3 = new TextContentParameter { Name = "p2", Value = "v2" };
+            prop.SetParameter(p3, "p3");
+            Assert.Equal(3, prop.ParameterCount);
+            Assert.NotNull(prop.FindParameter("P3"));
+
+            Assert.Same(p1, prop.FindParameter("P1"));
+            Assert.Same(p2, prop.FindParameter("P2"));
+            Assert.Same(p3, prop.FindParameter("P3"));
+            Assert.Null(prop.FindParameter("P4"));
+
+            Assert.NotNull(prop.FindParameter<TextContentParameter>("P1"));
+            Assert.Null(prop.FindParameter<TextContentParameter>("P2"));
+            Assert.NotNull(prop.FindParameter<TextContentParameter>("P3"));
+            Assert.Null(prop.FindParameter<TextContentParameter>("P4"));
+
+            Assert.Same(p3, prop.GetParameter<TextContentParameter>("p3"));
+            var p4 = prop.GetParameter<TextContentParameter>("p4");
+            Assert.NotNull(p4);
+            Assert.Equal(4, prop.ParameterCount);
+
+            var p5 = prop.GetParameter<TextContentParameter>("p2");
+            Assert.Same(p5, prop.FindParameter("P2"));
+            Assert.NotNull(prop.FindParameter<TextContentParameter>("P2"));
+            Assert.Equal(4, prop.ParameterCount);
+
+            prop.SetParameter(null, "p2");
+            Assert.Equal(3, prop.ParameterCount);
+            prop.SetParameter(null, "p3");
+            Assert.Equal(2, prop.ParameterCount);
+
+            Assert.Equal(new ContentParameter[] { p1, p4 }, prop.GetParameters());
+            prop.RemoveParameter("P1");
+            prop.RemoveParameter("P2");
+            Assert.Equal(new ContentParameter[] { p4 }, prop.GetParameters());
+
+            prop.RemoveParameter("p4");
+            Assert.Equal(0, prop.ParameterCount);
+
+            prop.SetParameter(p1);
+            prop.SetParameter(p2);
+            prop.SetParameter(p3);
+            prop.SetParameter(p4);
+            Assert.Equal(4, prop.ParameterCount);
+            prop.ClearParameters();
+            Assert.Equal(0, prop.ParameterCount);
+
+        }
+    }
+}
---
+++ sources/deuxsucres.ContentType/ContentProperties/ContentProperty.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace deuxsucres.ContentType
+{
+
+    /// <summary>
+    /// Content property
+    /// </summary>
+    public abstract class ContentProperty
+    {
+        Dictionary<string, ContentParameter> _parameters;
+
+        #region Parameters
+
+        /// <summary>
+        /// Remove all parameters
+        /// </summary>
+        public void ClearParameters()
+        {
+            _parameters = null;
+        }
+
+        /// <summary>
+        /// Define a parameter
+        /// </summary>
+        public void SetParameter(ContentParameter parameter, string name = null)
+        {
+            if (parameter != null)
+            {
+                if (!string.IsNullOrEmpty(name))
+                    parameter.Name = name;
+                if (_parameters == null)
+                    _parameters = new Dictionary<string, ContentParameter>(StringComparer.OrdinalIgnoreCase);
+                _parameters[parameter.Name] = parameter;
+            }
+            else if (!string.IsNullOrEmpty(name))
+                _parameters?.Remove(name);
+        }
+
+        /// <summary>
+        /// Find a parameter
+        /// </summary>
+        public ContentParameter FindParameter(string name)
+        {
+            ContentParameter result = null;
+            if (_parameters?.TryGetValue(name, out result) == true)
+                return result;
+            return null;
+        }
+
+        /// <summary>
+        /// Find a typed parameter
+        /// </summary>
+        public T FindParameter<T>(string name) where T : ContentParameter
+        {
+            return FindParameter(name) as T;
+        }
+
+        /// <summary>
+        /// Find or create a parameter if not exists
+        /// </summary>
+        public T GetParameter<T> (string name) where T : ContentParameter
+        {
+            T result = FindParameter<T>(name);
+            if (result == null)
+            {
+                result = Activator.CreateInstance<T>();
+                SetParameter(result, name);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Remove a parameter
+        /// </summary>
+        public void RemoveParameter(string name)
+        {
+            _parameters?.Remove(name);
+            if (_parameters?.Count == 0)
+                _parameters = null;
+        }
+
+        /// <summary>
+        /// List all parameters
+        /// </summary>
+        public IEnumerable<ContentParameter> GetParameters() 
+            => _parameters?.Values ?? Enumerable.Empty<ContentParameter>();
+
+        #endregion
+
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Count of parameters
+        /// </summary>
+        public int ParameterCount => _parameters?.Count ?? 0;
+    }
+
+}
