342c330504dd0aed362c783ad3dfeb0316dab6fd
Yanos <ygrenier@ygrenier.com>
2017-12-16 13:34:37 +0100

    Implements a text content parameter

---
+++ sources/deuxsucres.ContentType.Tests/ContentParameters/TextContentParameterTest.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.ContentType.Tests.ContentParameters
+{
+    public class TextContentParameterTest
+    {
+        [Fact]
+        public void Casts()
+        {
+            TextContentParameter param = new TextContentParameter();
+            param = "Value";
+
+            string s = param;
+            Assert.Equal("Value", s);
+
+            param = null;
+            s = param;
+            Assert.Null(s);
+
+            s = "Test";
+            param = s;
+            Assert.Null(param.Name);
+            Assert.Equal("Test", param.Value);
+
+            s = null;
+            param = s;
+            Assert.Null(param);
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            var syntax = new ContentSyntax();
+            TextContentParameter param = new TextContentParameter
+            {
+                Name = "Param",
+                Value = "Value"
+            };
+            var cparam = param.Serialize(syntax);
+            Assert.NotNull(cparam);
+            Assert.Equal("Param", cparam.Name);
+            Assert.Equal(new string[] { "Value" }, cparam.Values);
+            Assert.Equal("Value", cparam.Value);
+
+            cparam = new ContentLineParameter("Content");
+            cparam.Values.Add("Value 1");
+
+            param = new TextContentParameter();
+            param.Deserialize(cparam, syntax);
+            Assert.Equal("Content", param.Name);
+            Assert.Equal("Value 1", param.Value);
+
+            cparam.Values.Add("Value 2");
+
+            param = new TextContentParameter();
+            param.Deserialize(cparam, syntax);
+            Assert.Equal("Content", param.Name);
+            Assert.Equal("Value 1, Value 2", param.Value);
+        }
+    }
+}
---
+++ sources/deuxsucres.ContentType/ContentParameters/TextContentParameter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace deuxsucres.ContentType
+{
+    /// <summary>
+    /// Text content parameter
+    /// </summary>
+    public class TextContentParameter : ContentParameter
+    {
+        /// <summary>
+        /// Deserialize
+        /// </summary>
+        protected override bool InternalDeserialize(ContentLineParameter param, ContentSyntax syntax)
+        {
+            Value = param.Value;
+            return true;
+        }
+
+        /// <summary>
+        /// Serialize
+        /// </summary>
+        protected override void InternalSerialize(ContentLineParameter parameter, ContentSyntax syntax)
+        {
+            parameter.Value = Value;
+        }
+
+        /// <summary>
+        /// Cast to a string
+        /// </summary>
+        public static implicit operator string(TextContentParameter prop) { return prop?.Value; }
+
+        /// <summary>
+        /// Cast from a string
+        /// </summary>
+        public static implicit operator TextContentParameter(string value) { return value != null ? new TextContentParameter { Value = value } : null; }
+
+        /// <summary>
+        /// Value
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
