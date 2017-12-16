f9f93c53629c7a7b814929b83a93e3aba7cb1ae9
Yanos <ygrenier@ygrenier.com>
2017-12-16 14:57:50 +0100

    Implements a text list content parameter.

---
+++ sources/deuxsucres.ContentType.Tests/ContentParameters/TextListContentParameterTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.ContentType.Tests.ContentParameters
+{
+    public class TextListContentParameterTest
+    {
+        [Fact]
+        public void Casts()
+        {
+            TextListContentParameter param = new TextListContentParameter();
+            var values = new string[] { "Val1", "Val2" };
+
+            param = values;
+
+            string[] s = param;
+            Assert.Equal(values, s);
+            List<string> l = param;
+            Assert.Equal(values, l);
+
+            param = null;
+            s = param;
+            Assert.Null(s);
+            l = param;
+            Assert.Null(l);
+
+            s = new string[] { "Val1", "Val2" };
+            param = s;
+            Assert.Null(param.Name);
+            Assert.Equal(values, param.Value);
+            l = new List<string> { "Val1", "Val2" };
+            param = l;
+            Assert.Null(param.Name);
+            Assert.Equal(values, param.Value);
+
+            s = null;
+            param = s;
+            Assert.Null(param);
+
+            l = null;
+            param = l;
+            Assert.Null(param);
+        }
+
+        [Fact]
+        public void Serialization()
+        {
+            var syntax = new ContentSyntax();
+            TextListContentParameter param = new TextListContentParameter
+            {
+                Name = "Param",
+                Value = new List<string> { "Val1", "Val2" }
+            };
+            var cparam = param.Serialize(syntax);
+            Assert.NotNull(cparam);
+            Assert.Equal("Param", cparam.Name);
+            Assert.Equal(new string[] { "Val1", "Val2" }, cparam.Values);
+            Assert.Equal("Val1, Val2", cparam.Value);
+
+            cparam = new ContentLineParameter("Content");
+            cparam.Values.Add("Value 1");
+
+            param = new TextListContentParameter();
+            param.Deserialize(cparam, syntax);
+            Assert.Equal("Content", param.Name);
+            Assert.Equal(new string[] { "Value 1"}, param.Value);
+
+            cparam.Values.Add("Value 2");
+
+            param = new TextListContentParameter();
+            param.Deserialize(cparam, syntax);
+            Assert.Equal("Content", param.Name);
+            Assert.Equal(new string[] { "Value 1", "Value 2" }, param.Value);
+        }
+    }
+}
---
+++ sources/deuxsucres.ContentType/ContentParameters/TextListContentParameter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace deuxsucres.ContentType
+{
+    /// <summary>
+    /// List of text content parameter
+    /// </summary>
+    public class TextListContentParameter : ContentParameter
+    {
+        /// <summary>
+        /// Deserialize
+        /// </summary>
+        protected override bool InternalDeserialize(ContentLineParameter param, ContentSyntax syntax)
+        {
+            Value = new List<string>(param.Values);
+            return true;
+        }
+
+        /// <summary>
+        /// Serialize
+        /// </summary>
+        protected override void InternalSerialize(ContentLineParameter parameter, ContentSyntax syntax)
+        {
+            parameter.Values.Clear();
+            if (Value != null)
+                parameter.Values.AddRange(Value);
+        }
+
+        /// <summary>
+        /// Cast to a string array
+        /// </summary>
+        public static implicit operator string[](TextListContentParameter prop) { return prop?.Value?.ToArray(); }
+
+        /// <summary>
+        /// Cast from a string array
+        /// </summary>
+        public static implicit operator TextListContentParameter(string[] value) { return value != null ? new TextListContentParameter { Value = new List<string>(value ?? Enumerable.Empty<string>()) } : null; }
+
+        /// <summary>
+        /// Cast to a string list
+        /// </summary>
+        public static implicit operator List<string> (TextListContentParameter prop) { return prop?.Value?.ToList(); }
+
+        /// <summary>
+        /// Cast from a string list
+        /// </summary>
+        public static implicit operator TextListContentParameter(List<string> value) { return value != null ? new TextListContentParameter { Value = new List<string>(value ?? Enumerable.Empty<string>()) } : null; }
+
+        /// <summary>
+        /// Value
+        /// </summary>
+        public List<string> Value { get; set; }
+    }
+}
