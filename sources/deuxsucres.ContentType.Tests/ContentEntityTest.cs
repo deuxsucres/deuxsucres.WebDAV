da74bc36abcfcf8e28d685d6c8a209831f93c705
Yanos <ygrenier@ygrenier.com>
2017-12-17 09:28:01 +0100

    Components in a entity.

---
+++ sources/deuxsucres.ContentType.Tests/ContentEntityTest.cs
@@ -0,0 +1,100 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace deuxsucres.ContentType.Tests
+{
+    public class ContentEntityTest
+    {
+        class TestEntity : ContentEntity
+        {
+            public override string Name => "Test";
+        }
+        class TestProperty : ContentProperty
+        {
+        }
+
+        [Fact]
+        public void Create()
+        {
+            var entity = new Mock<ContentEntity>() { CallBase = true }.Object;
+            Assert.Equal(0, entity.ComponentCount);
+        }
+
+        [Fact]
+        public void Components()
+        {
+            var entity = new Mock<ContentEntity>() { CallBase = true }.Object;
+            Assert.Equal(0, entity.ComponentCount);
+
+            var c1 = new TestEntity();
+            var c2 = new TestProperty();
+            var c3 = new TestEntity();
+            var c4 = new TestProperty();
+
+            entity.AddComponent(c1);
+            entity.AddComponent(null);
+            Assert.Equal(1, entity.ComponentCount);
+
+            entity.AddComponent(c2);
+            Assert.Equal(2, entity.ComponentCount);
+            c2.Name = "Test";
+
+            entity.AddComponent(c3);
+            entity.AddComponent(c4);
+            c4.Name = "Property";
+
+            Assert.Equal(new IContentComponent[] { c1, c2, c3, c4 }, entity.GetComponents());
+            Assert.Same(c1, entity[0]);
+            Assert.Same(c4, entity[3]);
+            Assert.Equal(new IContentComponent[] { c1, c2, c3 }, entity.FindComponents("test"));
+            Assert.Equal(new IContentComponent[] { c1, c3 }, entity.FindComponents<ContentEntity>("test"));
+            Assert.Equal(new IContentComponent[] { c2 }, entity.FindComponents<ContentProperty>("test"));
+            Assert.Equal(new IContentComponent[] { c1, c3 }, entity.FindComponents<TestEntity>("TEST"));
+            Assert.Equal(new IContentComponent[] { c2 }, entity.FindComponents<TestProperty>("test"));
+
+            entity.RemoveComponents("TEST");
+            Assert.Equal(new IContentComponent[] { c4 }, entity.GetComponents());
+
+            entity.RemoveComponent(c1);
+            Assert.Equal(new IContentComponent[] { c4 }, entity.GetComponents());
+            entity.RemoveComponent(null);
+            Assert.Equal(new IContentComponent[] { c4 }, entity.GetComponents());
+            entity.RemoveComponent(c4);
+            Assert.Equal(new IContentComponent[] { }, entity.GetComponents());
+
+            entity.AddComponent(c1);
+            entity.AddComponent(c2);
+            entity.AddComponent(c3);
+            entity.AddComponent(c4);
+
+            entity.SetComponent(null);
+            entity.SetComponent(c3);
+            Assert.Equal(new IContentComponent[] { c4, c3 }, entity.GetComponents());
+
+            entity.SetProperty(c4, "Test");
+            Assert.Equal("Test", c4.Name);
+            Assert.Equal(new IContentComponent[] { c4 }, entity.GetComponents());
+
+            Assert.Same(c4, entity.GetProperty<TestProperty>("test"));
+            var c5 = entity.GetProperty<TestProperty>("Prop");
+            Assert.NotNull(c5);
+            Assert.Equal("Prop", c5.Name);
+            Assert.Equal(new IContentComponent[] { c4, c5 }, entity.GetComponents());
+
+            entity.SetProperty(null, null);
+            Assert.Equal(new IContentComponent[] { c4, c5 }, entity.GetComponents());
+            entity.SetProperty(null, "prop");
+            Assert.Equal(new IContentComponent[] { c4 }, entity.GetComponents());
+
+            entity.ClearComponents();
+            Assert.Equal(new IContentComponent[] { }, entity.GetComponents());
+
+        }
+
+    }
+}
--- sources/deuxsucres.ContentType/ContentEntity.cs
+++ sources/deuxsucres.ContentType/ContentEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace deuxsucres.ContentType
@@ -9,13 +10,122 @@ namespace deuxsucres.ContentType
     /// </summary>
     public abstract class ContentEntity : IContentComponent
     {
+        List<IContentComponent> _components = new List<IContentComponent>();
+
         #region IContentComponent
         ContentEntity IContentComponent.Parent { get; set; }
+        #endregion
+
+        #region Components
+
+        /// <summary>
+        /// Clear the components
+        /// </summary>
+        public void ClearComponents()
+        {
+            _components.Clear();
+        }
+
+        /// <summary>
+        /// Add a component
+        /// </summary>
+        public void AddComponent(IContentComponent component)
+        {
+            if (component != null && !_components.Contains(component))
+                _components.Add(component);
+        }
+
+        /// <summary>
+        /// Define a component
+        /// </summary>
+        public void SetComponent(IContentComponent component)
+        {
+            if (component == null) return;
+            RemoveComponents(component.Name);
+            AddComponent(component);
+        }
+
+        /// <summary>
+        /// Define a property
+        /// </summary>
+        public void SetProperty(ContentProperty property, string name = null)
+        {
+            if (property != null)
+            {
+                if (!string.IsNullOrEmpty(name))
+                    property.Name = name;
+                SetComponent(property);
+            }
+            else if (!string.IsNullOrEmpty(name))
+                RemoveComponents(name);
+        }
+
+        /// <summary>
+        /// Find or create a property if not exists
+        /// </summary>
+        public T GetProperty<T>(string name) where T : ContentProperty
+        {
+            T result = FindComponents<T>(name).FirstOrDefault();
+            if (result == null)
+            {
+                result = Activator.CreateInstance<T>();
+                SetProperty(result, name);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Find components from a name
+        /// </summary>
+        public IEnumerable<IContentComponent> FindComponents(string name)
+            => _components.Where(c => string.Equals(name, c.Name, StringComparison.OrdinalIgnoreCase));
+
+        /// <summary>
+        /// Find typed components from a name
+        /// </summary>
+        public IEnumerable<T> FindComponents<T>(string name) where T : IContentComponent
+            => FindComponents(name).OfType<T>();
+
+        /// <summary>
+        /// List the components
+        /// </summary>
+        public IEnumerable<IContentComponent> GetComponents() => _components;
+
+        /// <summary>
+        /// Remove a component
+        /// </summary>
+        public void RemoveComponent(IContentComponent component)
+        {
+            _components.Remove(component);
+        }
+
+        /// <summary>
+        /// Remove all components from a name
+        /// </summary>
+        public void RemoveComponents(string name)
+        {
+            for (int i = _components.Count - 1; i >= 0; i--)
+            {
+                if (string.Equals(name, _components[i].Name, StringComparison.OrdinalIgnoreCase))
+                    _components.RemoveAt(i);
+            }
+        }
+
         #endregion
 
         /// <summary>
         /// Name
         /// </summary>
         public abstract string Name { get; }
+
+        /// <summary>
+        /// Component access
+        /// </summary>
+        public IContentComponent this[int idx] { get => _components[idx]; }
+
+        /// <summary>
+        /// Count of components
+        /// </summary>
+        public int ComponentCount => _components.Count;
     }
 }
